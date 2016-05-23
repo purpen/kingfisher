364e52e0d821cd983c77e74cf7c0d27bc377142b
caowei <caoyuanlianni@foxmail.com>
2016-05-23 19:44:07 +0800

    add resources

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-var elixir = require('laravel-elixir');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,6 +10,35 @@ var elixir = require('laravel-elixir');
  |
  */
 
+var elixir = require('laravel-elixir');
+
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass([
+        'bootstrap.scss',
+    ], 'public/assets/css/bootstrap.css');
+});
+
+elixir(function(mix) {
+    mix
+        .styles(['font-awesome.css'], 'public/assets/css/font-awesome.css')
+        .styles(['fonts.css'], 'public/assets/css/fonts.css')
+        .styles(['app.css'], 'public/assets/css/app.css');
+});
+
+elixir(function(mix) {
+    mix
+        .scripts(['jquery.js'], 'public/assets/js/jquery.js')
+        .scripts(['app.js'], 'public/assets/js/app.js');
+});
+
+elixir(function(mix) {
+    mix
+        .copy('node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js', 'public/assets/js/bootstrap.js')
+        .copy('resources/assets/fonts/', 'public/build/assets/fonts/')
+        .copy('node_modules/bootstrap-sass/assets/fonts/bootstrap/', 'public/build/assets/fonts/');
+});
+
+// 版本号码缓存必须放在编译之后
+elixir(function(mix) {
+    mix.version(['assets/css/', 'assets/js/']);
 });
---
+++ resources/assets/css/app.css
@@ -0,0 +1,5 @@
+/*!
+ * myblog v1.0 
+ * Copyright 2015 CAOWEI, Inc.
+ * Licensed under the MIT license
+ */
