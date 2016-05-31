345af72d799e183b6138ea23a87cda6d520d4832
caowei <caoyuanlianni@foxmail.com>
2016-05-31 18:18:01 +0800

    add login and res page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,35 +4,8 @@
 
 @section('customize_css')
     @parent
-        .control-label{
-            color: #666;
-        }
-        #container{
-            background:url({{ url('images/default/background.jpg') }}) no-repeat;
-            background-size:cover;
-            position: relative;
-        }
-        #erp-content{
-            min-height:300px;
-        }
         #login-block{
             width: 500px;
-            height: 305px;
-            padding: 0 30px;
-            background: #fff;
-            position: absolute;
-            top:0;
-            right:15%;
-        }
-        #login-block h3{
-            padding-bottom: 15px;
-            color: #FF3366 !important;
-        }
-        .erp-login{
-            width: 100%;
-        }
-        .forgetpass{
-            padding-top: 6px;
         }
         .erp-verify{
             padding-left: 6px;
