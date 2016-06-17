5cc283e929e42b569dcbc14aac2b3bc43f662481
liliangheng <liheng666@foxmail.com>
2016-06-17 18:18:31 +0800

    some files

--- app/Models/StorageModel.php
+++ app/Models/StorageModel.php
@@ -3,9 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use App\Models\StorageModel;
 class StorageModel extends Model
 {
     use SoftDeletes;
---
+++ database/migrations/2016_06_17_075930_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     *
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',20);
+            $table->integer('pid')->default(0);
+            $table->integer('order')->default(0);
+            $table->tinyInteger('type')->default(1);
+            $table->tinyInteger('status')->default(1);
+            $table->string('summary',500)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('categories');
+    }
+}
