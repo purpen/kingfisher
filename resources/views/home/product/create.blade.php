fa9842626dd9790202f242b34f01773663aa3fb0
lyf <987083180@qq.com>
2016-06-24 20:56:08 +0800

    creat

--- resources/views/home/product/create.blade.php
+++ resources/views/home/product/create.blade.php
@@ -14,7 +14,7 @@
 	.img-add{
 	    width: 100px;
 	    height: 100px;
-	    background: #ccc;
+	    background: #f5f5f5;
 	    vertical-align: middle;
 	    text-align: center;
 	    padding: 24px 0;
@@ -73,140 +73,149 @@
 		</div>
 	</div>
 	<div class="container mainwrap">
-		<div class="row mb-2r">
-			<div class="col-md-12">
-				<h5>商品分类</h5>
+		<form id="addproduct" role="form">
+			<div class="row mb-0 ui white pt-3r pb-2r">
+				<div class="col-md-12">
+					<h5>商品分类</h5>
+				</div>
 			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-8">
-				<form class="form-inline" role="form">
-					<div class="form-group">请选择商品分类：</div>
-					<div class="form-group">
-						<select class="selectpicker" id="orderType" style="display: none;">
-							<option value="0">未分类</option>
-							<option value=" ">商品</option>
-							<option value=" ">未分类</option>
-						</select>
+			<div class="row ui white pb-4r">
+				<div class="col-md-8">
+					<div class="form-inline">
+						<div class="form-group">请选择商品分类：</div>
+						<div class="form-group">
+							<select class="selectpicker" id="orderType" style="display: none;">
+								<option value="0">未分类</option>
+								<option value=" ">商品</option>
+								<option value=" ">未分类</option>
+							</select>
+						</div>
 					</div>
-				</form>
+				</div>
 			</div>
-		</div>
-		<div class="row mb-2r">
-			<div class="col-md-12">
-				<h5>基本信息</h5>
+			<div class="row mb-0 pt-3r pb-2r ui white">
+				<div class="col-md-12">
+					<h5>基本信息</h5>
+				</div>
 			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-4">
-				<form class="form-inline" role="form">
-					<div class="form-group m-92">货号：</div>
-					<div class="form-group">
-						<input type="text" name="b2cCode" ordertype="b2cCode" class="form-control" id="b2cCode">
+			<div class="row mb-0 pb-4r ui white">
+				<div class="col-md-4">
+					<div class="form-inline">
+						<div class="form-group m-92">货号：</div>
+						<div class="form-group">
+							<input type="text" name="b2cCode" ordertype="b2cCode" class="form-control" id="b2cCode">
+						</div>
 					</div>
-				</form>
-			</div>
-			<div class="col-md-4">
-				<form class="form-inline" role="form">
-					<div class="form-group m-92">商品名称：</div>
-					<div class="form-group">
-						<input type="text" name="b2cCode" ordertype="b2cCode" class="form-control" id="b2cCode">
+				</div>
+				<div class="col-md-4">
+					<div class="form-inline">
+						<div class="form-group m-92">商品名称：</div>
+						<div class="form-group">
+							<input type="text" name="b2cCode" ordertype="b2cCode" class="form-control" id="b2cCode">
+						</div>
 					</div>
-				</form>
+				</div>
 			</div>
-		</div>
-		<div class="row mb-3r">
-			<div class="col-md-4">
-				<form class="form-inline" role="form">
-					<div class="form-group m-92">标准售价(元)：</div>
-					<div class="form-group">
-						<input type="text" name="b2cCode" ordertype="b2cCode" class="form-control" id="b2cCode">
+			<div class="row pb-4r ui white">
+				<div class="col-md-4">
+					<div class="form-inline">
+						<div class="form-group m-92">标准售价(元)：</div>
+						<div class="form-group">
+							<input type="text" name="b2cCode" ordertype="b2cCode" class="form-control" id="b2cCode">
+						</div>
 					</div>
-				</form>
-			</div>
-			<div class="col-md-4">
-				<form class="form-inline" role="form">
-					<div class="form-group m-92">重量(kg)：</div>
-					<div class="form-group">
-						<input type="text" name="b2cCode" ordertype="b2cCode" class="form-control" id="b2cCode">
+				</div>
+				<div class="col-md-4">
+					<div class="form-inline">
+						<div class="form-group m-92">重量(kg)：</div>
+						<div class="form-group">
+							<input type="text" name="b2cCode" ordertype="b2cCode" class="form-control" id="b2cCode">
+						</div>
 					</div>
-				</form>
+				</div>
 			</div>
-		</div>
-		<div class="row mb-2r">
-			<div class="col-md-12">
-				<h5>商品图片</h5>
+			<div class="row mb-0 pt-3r pb-2r ui white">
+				<div class="col-md-12">
+					<h5>商品图片</h5>
+				</div>
 			</div>
-		</div>
-		<div class="row mb-2r addcol">
-			<div class="col-md-2 mb-3r">
-				<img src="" style="width: 100px;height: 100px;" class="img-thumbnail">
-				<a class="removeimg">删除</a>
-			</div>
-			<div class="col-md-2 mb-3r">
-				<a data-toggle="modal" data-target="#addimg" style="text-decoration: none;">
-					<div class="img-add">
-						<span class="glyphicon glyphicon-plus f46"></span>
-						<p>添加图片</p>
-					</div>
-				</a>
-				<div class="modal fade" id="addimg" tabindex="-1" role="dialog" aria-labelledby="addimgLabel">
-					<div class="modal-dialog modal-zm" role="document">
-						<div class="modal-content">
-							<div class="modal-header">
-								<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-								<h4 class="modal-title" id="gridSystemModalLabel">添加图片</h4>
-							</div>
-							<div class="modal-body">
-								<ul class="nav nav-tab" role="tablist">
-									<li class="active">
-										<a data-target="#one" role="tab" data-toggle="tab">网络图片地址</a>
-									</li>
-									<li>
-							            <a data-target="#two" role="tab" data-toggle="tab">
-							            <span class="glyphicon glyphicon-link"></span> 添加本地图片</a>
-							        </li>
-								</ul>
-								<div class="tab-content">
-									<div class="tab-pane active" id="one">
-										<div class="mt-3r">
-											<input type="text" name="picUrl" placeholder="请输入网络图片地址" class="form-control">
+			<div class="row mb-2r addcol pb-4r ui white">
+				<div class="col-md-2 mb-3r">
+					<img src="" style="width: 100px;height: 100px;" class="img-thumbnail">
+					<a class="removeimg">删除</a>
+				</div>
+				<div class="col-md-2 mb-3r">
+					<a data-toggle="modal" data-target="#addimg" style="text-decoration: none;">
+						<div class="img-add">
+							<span class="glyphicon glyphicon-plus f46"></span>
+							<p>添加图片</p>
+						</div>
+					</a>
+					<div class="modal fade" id="addimg" tabindex="-1" role="dialog" aria-labelledby="addimgLabel">
+						<div class="modal-dialog modal-zm" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+									<h4 class="modal-title" id="gridSystemModalLabel">添加图片</h4>
+								</div>
+								<div class="modal-body">
+									<ul class="nav nav-tab" role="tablist">
+										<li class="active">
+											<a data-target="#one" role="tab" data-toggle="tab">网络图片地址</a>
+										</li>
+										<li>
+								            <a data-target="#two" role="tab" data-toggle="tab">
+								            <span class="glyphicon glyphicon-link"></span> 添加本地图片</a>
+								        </li>
+									</ul>
+									<div class="tab-content">
+										<div class="tab-pane active" id="one">
+											<div class="mt-3r">
+												<input type="text" name="picUrl" placeholder="请输入网络图片地址" class="form-control">
+											</div>
 										</div>
-									</div>
-									<div class="tab-pane" id="two">
-										<div class="mt-3r">
-											<form id="picForm" enctype="multipart/form-data">
-								                <input  type="file" name="picUrl" placeholder="添加本地图片" class="form-control">
-								                <div class="filename">
-								                	点击选择文件
-								                </div>
-								            </form>
+										<div class="tab-pane" id="two">
+											<div class="mt-3r">
+												<form id="picForm" enctype="multipart/form-data">
+									                <input  type="file" name="picUrl" placeholder="添加本地图片" class="form-control">
+									                <div class="filename">
+									                	点击选择文件
+									                </div>
+									            </form>
+											</div>
 										</div>
 									</div>
 								</div>
-							</div>
-							<div class="modal-footer">
-								<button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
-								<button id="addpicUrl" type="submit" class="btn btn-magenta">确定</button>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+									<button id="addpicUrl" type="submit" class="btn btn-magenta">确定</button>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div class="modal fade" id="Modalerror" tabindex="-1" role="dialog" aria-labelledby="ModalerrorLabel">
-					<div class="modal-dialog" role="document">
-						<div class="modal-content">
-							<div class="modal-body">
-								<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-								<div class="bootbox-body">请上传本地图片或输入网络图片地址</div>
-							</div>
-							<div class="modal-footer">
-								<button data-dismiss="modal" type="button" class="btn btn-magenta">OK</button>
+					<div class="modal fade" id="Modalerror" tabindex="-1" role="dialog" aria-labelledby="ModalerrorLabel">
+						<div class="modal-dialog" role="document">
+							<div class="modal-content">
+								<div class="modal-body">
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+									<div class="bootbox-body">请上传本地图片或输入网络图片地址</div>
+								</div>
+								<div class="modal-footer">
+									<button data-dismiss="modal" type="button" class="btn btn-magenta">OK</button>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+
+			<div class="row mt-4r pt-2r">
+				<button type="button" class="btn btn-magenta mr-r save">保存</button>
+				<button type="button" class="btn btn-white cancel once">取消</button>
+			</div>
+		</form>
+
+		{{--
 		<div class="row mb-2r">
 			<div class="col-md-12">
 				<h5>
@@ -252,6 +261,8 @@
 				</table>
 			</div>
 		</div>
+		--}}
+
 	</div>
 	
 @endsection
