eb546661b9a8c8b32479d9ca5605e0c3a215ec7e
liliangheng <liheng666@foxmail.com>
2016-07-06 17:30:24 +0800

    change

--- config/qiniu.php
+++ config/qiniu.php
@@ -1,8 +1,8 @@
 <?php
     return [
-        'access_key' => 'ERh7qjVSy0v42bQ0fftrFeKYZG39XbzRlaJO4NFy',                                  //用户公钥
-        'secret_ket' => 'r-NUrKsnRBEwTQxbLONVrK9tPuncXyHmcq4BkSc7',                                  //私钥
-        'bucket_name' => 'fravatar',                                 //储存空间
+        'access_key' => 'AWTEpwVNmNcVjsIL-vS1hOabJ0NgIfNDzvTbDb4i',                                  //用户公钥
+        'secret_ket' => 'F_g7diVuv1X4elNctf3o3bNjhEAe5MR3hoCk7bY6',                                  //私钥
+        'bucket_name' => 'frbird',                               //储存空间
         'call_back_url' => 'http://erp.taihuoniao.com/asset/callback',             //服务器回调url
         'domain' => 'erp',                                              //存储域
         'url' => 'fravatar.qiniudn.com',                           //图片服务器
