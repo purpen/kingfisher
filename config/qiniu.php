808e6e71dd69c6c2dffac6a84936116f09474cc7
liliangheng <liheng666@foxmail.com>
2016-07-06 20:18:59 +0800

    change

--- config/qiniu.php
+++ config/qiniu.php
@@ -5,5 +5,5 @@
         'bucket_name' => 'frking',                                      //储存空间
         'call_back_url' => 'http://erp.taihuoniao.com/asset/callback',  //服务器回调url
         'domain' => 'erp',                                              //存储域
-        'url' => 'http://o9w00gtvf.bkt.clouddn.com'                     //图片服务器
+        'url' => 'http://o9w00gtvf.bkt.clouddn.com/'                     //图片服务器
     ];
