4308f83630f58fc45915cc44fb82987bc3df4270
liliangheng <liheng666@foxmail.com>
2016-07-08 16:23:38 +0800

    change

---
+++ app/Models/CountersModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CountersModel extends Model
+{
+    /**
+     * 关联模型到数据表
+     * @var string
+     */
+    protected $table = 'counters';
+}
---
+++ database/migrations/2016_07_08_065635_create_counters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCountersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('counters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',20);
+            $table->integer('mark')->default(0);
+            $table->integer('val')->default(0);
+            $table->index(['name','mark']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('counters');
+    }
+}
