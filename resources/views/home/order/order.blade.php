34ae67786138d96cfdbb82e2d8fd80a595173c61
dafengniao <987083180@qq.com>
2016-08-04 11:59:17 +0800

    order

---
+++ resources/views/home/order/order.blade.php
@@ -0,0 +1,15 @@
+@extends('home.base')
+
+@section('title', 'console')
+@section('customize_css')
+    @parent
+
+@endsection
+@section('content')
+    @parent
+
+@endsection
+@section('customize_js')
+    @parent
+
+@endsection
