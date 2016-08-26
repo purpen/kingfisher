91707f3e553bbf938f10389d111f2ce2355645c9
liliangheng <liheng666@foxmail.com>
2016-08-26 14:58:24 +0800

    change

--- app/Http/Middleware/AuthMiddleware.php
+++ app/Http/Middleware/AuthMiddleware.php
@@ -16,7 +16,7 @@ class AuthMiddleware
         $path = $request->path();
         $user = Auth::user();
 //        $user = UserModel::find(session('account'));
-        dd($user);
+        //dd($user);
         // dd($user->may('admin/user/index')); //测试时 已经把权限关闭 实现效果 放开注释
         // if(!$user->may($path)){
         // abort(403);
