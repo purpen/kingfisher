388f69cbed4b930588de9242477685497da1e21b
purpen <purpen.w@gmail.com>
2016-10-29 17:08:01 +0800

    update changewarehouse style

--- resources/views/home/storage/changeWarehouse.blade.php
+++ resources/views/home/storage/changeWarehouse.blade.php
@@ -20,107 +20,101 @@
         <div class="row fz-0">
 			<div class="form-inline">
 				<div class="form-group mr-2r">
-					<a href="{{ url('/order/create') }}" class="btn btn-white">
-						<i class="glyphicon glyphicon-edit"></i> 创建订单
-					</a>
-                    <button type="button" id="batch-verify" class="btn btn-white mlr-2r">
+                    <a href="{{ url('/changeWarehouse/create') }}" class="btn btn-white">
+                        <i class="glyphicon glyphicon-edit"></i> 创建调拨单
+                    </a>
+                    <button type="button" id="batch-verify" class="btn btn-white mlr-r">
                         <i class="glyphicon glyphicon-ok"></i> 批量审批
                     </button>
-                    <button type="button" id="batch-reversed" class="btn btn-white mlr-2r">
-                        <i class="glyphicon glyphicon-arrow-left"></i> 批量反审
-                    </button>
-                    <button type="button" class="btn btn-white" id="send-order">
-                        <i class="glyphicon glyphicon-print"></i> 打印发货
+                    <button type="button" id="batch-remove" class="btn btn-white mlr-r">
+                        <i class="glyphicon glyphicon-trash"></i> 删除
                     </button>
 				</div>
 				<div class="form-group mr-2r">
 					<button type="button" class="btn btn-gray">
 						<i class="glyphicon glyphicon-arrow-up"></i> 导出
 					</button>
-					<button type="button" class="btn btn-gray">
+					<button type="button" class="btn btn-gray ml-r">
 						<i class="glyphicon glyphicon-arrow-down"></i> 导入
 					</button>
 				</div>
 			</div>
-            
-            <a href="{{ url('/changeWarehouse/create') }}" class="btn btn-white">>
-                <i class="glyphicon glyphicon-edit"></i> 创建调拨单
-            </a>
-            
-			<div class="form-group mr-2r">
-				<button type="button" class="btn btn-gray">
-					<i class="glyphicon glyphicon-arrow-up"></i> 导出
-				</button>
-				<button type="button" class="btn btn-gray">
-					<i class="glyphicon glyphicon-arrow-down"></i> 导入
-				</button>
-			</div>
-            
-            <button type="button" class="btn btn-white mlr-2r">导出</button>
-            <button type="button" class="btn btn-white">导入</button>
         </div>
-        <div class="row">
-            <div class="row scroll">
-                <table class="table table-bordered table-striped">
-                    <thead>
-                    <tr class="gblack">
-                        <th class="text-center"><input type="checkbox" id="checkAll"></th>
-                        <th>调拨单编号</th>
-                        <th>创建时间</th>
-                        <th>创建人</th>
-                        <th>审核人</th>
-                        <th>审核时间</th>
-                        <th>调出仓库</th>
-                        <th>调入仓库</th>
-                        <th>调拨状态</th>
-                        <th>备注</th>
-                        <th>操作</th>
-                        <th>
-                        </th>
+        
+        <div class="row scroll">
+            <table class="table table-bordered table-striped">
+                <thead>
+                <tr class="gblack">
+                    <th class="text-center"><input type="checkbox" id="checkAll"></th>
+                    <th class="text-center">调拨状态</th>
+                    <th>调拨单编号</th>
+                    <th>创建时间</th>
+                    <th>创建人</th>
+                    <th>审核人</th>
+                    <th>审核时间</th>
+                    <th>调出仓库</th>
+                    <th>调入仓库</th>
+                    <th>备注</th>
+                    <th>操作</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($change_warehouse as $purchase)
+                    <tr>
+                        <td class="text-center"><input name="Order" type="checkbox"></td>
+                        <td class="text-center">
+                            <span class="label label-danger">{{$purchase->storage_status}}</span>
+                        </td>
+                        <td class="magenta-color">{{$purchase->number}}</td>
+                        <td>{{$purchase->created_at_val}}</td>
+                        <td>{{$purchase->user_name}}</td>
+                        <td>{{$purchase->verify_name}}</td>
+                        <td>{{$purchase->updated_at}}</td>
+                        <td>{{$purchase->out_storage_name}}</td>
+                        <td>{{$purchase->in_storage_name}}</td>
+                        <td>{{$purchase->summary}}</td>
+                        <td tdr="nochect">
+                            <a href="{{url('/changeWarehouse/show')}}?id={{$purchase->id}}" class="btn btn-default btn-sm mr-r">查看</a>
+                            @if (!$purchase->verified)
+                            <button type="button" id="change-status" value="{{$purchase->id}}" class="btn btn-white btn-sm mr-r">
+                                <i class="glyphicon glyphicon-ok"></i> 确认调拨
+                            </button>
+                            <a href="{{url('/changeWarehouse/edit')}}?id={{$purchase->id}}" class="btn btn-default btn-sm mr-r">编辑</a>
+                            @endif
+                            
+                            @if ($purchase->verified == 1)
+                            <button type="button" id="verify-status" value="{{$purchase->id}}" class="btn btn-white btn-sm mr-r">
+                                <i class="glyphicon glyphicon-ok"></i> 主管审批
+                            </button>
+                            @endif
+                        </td>
                     </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($change_warehouse as $purchase)
-                        <tr>
-                            <td class="text-center"><input name="Order" type="checkbox"></td>
-                            <td class="magenta-color">{{$purchase->number}}</td>
-                            <td>{{$purchase->created_at_val}}</td>
-                            <td>{{$purchase->user_name}}</td>
-                            <td>{{$purchase->verify_name}}</td>
-                            <td>{{$purchase->updated_at}}</td>
-                            <td>{{$purchase->out_storage_name}}</td>
-                            <td>{{$purchase->in_storage_name}}</td>
-                            <td>{{$purchase->storage_status}}</td>
-                            <td>{{$purchase->summary}}</td>
-                            <td tdr="nochect"><button type="button" id="change-status" value="{{$purchase->id}}" class="btn btn-white btn-sm mr-r">审核通过</button>
-                                <a href="{{url('/changeWarehouse/show')}}?id={{$purchase->id}}" class="magenta-color mr-r">详细</a>
-                                <a href="{{url('/changeWarehouse/edit')}}?id={{$purchase->id}}" class="magenta-color mr-r">编辑</a>
-                                <a href="javascript:void(0)" value="{{$purchase->id}}" class="magenta-color delete">删除</a>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
-            @if ($change_warehouse)
-                <div class="col-md-6 col-md-offset-6">{!! $change_warehouse->render() !!}</div>
-            @endif
+                @endforeach
+                </tbody>
+            </table>
         </div>
-        <input type="hidden" id="_token" name="_token" value="<?php echo csrf_token(); ?>">
+        @if ($change_warehouse)
+        <div class="row">
+            <div class="col-md-6 col-md-offset-6">{!! $change_warehouse->render() !!}</div>
+        </div>
+        @endif
+    </div>
+    <input type="hidden" id="_token" name="_token" value="<?php echo csrf_token(); ?>">
 @endsection
 
 
 @section('customize_js')
-    {{--<script>--}}
     @parent
+    
     var _token = $("#_token").val();
+    
     $(".delete").click(function () {
         if(confirm('确认删除该订单？')){
             var id = $(this).attr('value');
             var de = $(this);
             $.post('{{url('/changeWarehouse/ajaxDestroy')}}',{'_token':_token,'id':id},function (e) {
                 if(e.status){
-                de.parent().parent().remove();
+                    de.parent().parent().remove();
                 }
             },'json');
         }
@@ -136,4 +130,16 @@
             }
         },'json');
     });
+    
+    $('#verify-status').click(function () {
+        var id = $(this).attr('value');
+        $.post("{{url('/changeWarehouse/ajaxDirectorVerified')}}",{'_token':_token,'id':id},function (e) {
+            if(e.status){
+                location.reload();
+            }else if(e.status == 0){
+                alert(e.message);
+            }
+        },'json');
+    });
+    
 @endsection
--- resources/views/home/storage/exchange_subnav.blade.php
+++ resources/views/home/storage/exchange_subnav.blade.php
@@ -1,13 +1,23 @@
 <ul class="nav navbar-nav nav-list">
-    <li class="active"><a href="{{url('/changeWarehouse')}}">待审核 ({{$count_arr['count_0']}})</a></li>
-    <li><a href="{{url('/changeWarehouse/verify')}}">业管主管审核 ({{$count_arr['count_1']}})</a></li>
-    <li><a href="{{url('/changeWarehouse/completeVerify')}}">审核已完成</a></li>
+    <li @if($tab_menu == 'waitcheck')class="active"@endif>
+        <a href="{{url('/changeWarehouse')}}">
+            待审核 @if($tab_count['waitcheck'])<span class="badge">{{$tab_count['waitcheck']}}</span>@endif
+        </a>
+    </li>
+    <li @if($tab_menu == 'verified')class="active"@endif>
+        <a href="{{url('/changeWarehouse/verify')}}">
+            业管主管审核 @if($tab_count['verifing'])<span class="badge">{{$tab_count['verifing']}}</span>@endif
+        </a>
+    </li>
+    <li @if($tab_menu == 'finished')class="active"@endif>
+        <a href="{{url('/changeWarehouse/completeVerify')}}">审核已完成</a>
+    </li>
 </ul>
 <ul class="nav navbar-nav navbar-right mr-0">
     <li class="dropdown">
-        <form class="navbar-form navbar-left" role="search" id="search" action="#" method="POST">
+        <form class="navbar-form navbar-left" role="search" id="search" action="{{ url('/changeWarehouse/search') }}" method="POST">
             <div class="form-group">
-                <input type="text" name="where" class="form-control" placeholder="">
+                <input type="text" name="q" class="form-control" placeholder="编号">
                 <input type="hidden" id="_token" name="_token" value="<?php echo csrf_token(); ?>">
             </div>
             <button id="purchase-search" type="submit" class="btn btn-default">搜索</button>
