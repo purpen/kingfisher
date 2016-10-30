7377497ccc4c0f290260da42918be68302f056a7
purpen <purpen.w@gmail.com>
2016-10-30 20:15:22 +0800

    update qiniu config

--- config/qiniu.php
+++ config/qiniu.php
@@ -3,7 +3,7 @@
         'access_key' => 'AWTEpwVNmNcVjsIL-vS1hOabJ0NgIfNDzvTbDb4i',      //用户公钥
         'secret_key' => 'F_g7diVuv1X4elNctf3o3bNjhEAe5MR3hoCk7bY6',     //私钥
         'bucket_name' => 'frking',                                      //储存空间
-        'call_back_url' => 'http://erp.taihuoniao.com/asset/callback',  //服务器回调url
+        'call_back_url' => 'https://erp.taihuoniao.com/asset/callback',  //服务器回调url
         'domain' => 'erp',                                              //存储域
         'url' => 'http://o9w00gtvf.bkt.clouddn.com/',                    //图片服务器
         'small' => '-small.jpg'              //缩略图
