8b64ef732f429e414864e1fd1d391c27de081dbf
purpen <purpen.w@gmail.com>
2016-10-30 22:30:38 +0800

    fixed bug

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -7,6 +7,7 @@ use App\Models\PositiveEnergyModel;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Auth;
+
 class IndexController extends Controller
 {
     /**
@@ -16,33 +17,38 @@ class IndexController extends Controller
      */
     public function index(Request $request)
     {
+        $content = '';
+        
         // 显示短语
         $date = date('H');
         if ($date > 0 and $date < 8) {
             $date = 1;
         } elseif ($date > 8 and $date < 12) {
             $date = 2;
-        }elseif($date > 12 and $date < 18){
+        } elseif($date > 12 and $date < 18) {
             $date = 3;
-        }elseif($date > 18 and $date < 24){
+        } elseif($date > 18 and $date < 24) {
             $date = 4;
         }
-
+        
         $sex = Auth::user()->sex;
         if (!$sex) {
-            $positiveEnergys = PositiveEnergyModel::orderBy('id','desc')->get();
+            $positiveEnergys = PositiveEnergyModel::orderBy('id', 'desc')->get();
         }else{
-            $positiveEnergys = PositiveEnergyModel::where('type',$date)->where('sex',$sex)->get();
+            $positiveEnergys = PositiveEnergyModel::where('type', $date)->where('sex',$sex)->get();
         }
-
+        
         $contents = [];
-        foreach($positiveEnergys as $positiveEnergy){
+        foreach ($positiveEnergys as $positiveEnergy) {
             $contents[] = $positiveEnergy->content;
         }
         
-        $k = array_rand($contents);
-
-        return view('home.index', ['content'=>$contents[$k]]);
+        if (!empty($contents)) {
+            $k = array_rand($contents);
+            $content = $contents[$k];
+        }
+        
+        return view('home.index', ['content' => $content]);
     }
 
 
