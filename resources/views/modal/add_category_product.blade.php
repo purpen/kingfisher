de398763a6036a36a43d8059c4f33f8d99fa3aee
clg <731992627@qq.com>
2016-11-02 17:34:20 +0800

    404

---
+++ resources/views/modal/add_category_product.blade.php
@@ -0,0 +1,55 @@
+<div class="modal fade" id="addclass" tabindex="-1" role="dialog" aria-labelledby="addclassLabel">
+    <div class="modal-dialog modal-zm" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="gridSystemModalLabel">新增分类</h4>
+            </div>
+            <div class="modal-body">
+                <form id="addclassify" class="form-horizontal" role="form" method="POST" action="{{ url('/category/store') }}">
+                    {!! csrf_field() !!}
+                    <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+                        <label for="title" class="col-sm-2 control-label p-0 lh-34 m-56">分类名</label>
+                        <div class="col-sm-8">
+                            <input type="text" name="title" class="form-control float" id="title" placeholder="输入分类名"  value="{{ old('title') }}">
+                            @if ($errors->has('title'))
+                                <span class="help-block">
+														<strong>{{ $errors->first('title') }}</strong>
+													</span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="form-group{{ $errors->has('order') ? ' has-error' : '' }}">
+                        <label for="order" class="col-sm-2 control-label p-0 lh-34 m-56">排序</label>
+                        <div class="col-sm-8">
+                            <input type="text" name="order" class="form-control float" id="order" placeholder="选填"  value="{{ old('order') }}">
+                            @if ($errors->has('order'))
+                                <span class="help-block">
+														<strong>{{ $errors->first('order') }}</strong>
+													</span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="type" class="col-sm-2 control-label p-0 lh-34 m-56">类型</label>
+                        <div class="col-md-8 pl-4r ml-3r">
+                            <div class="form-inline">
+                                <div class="form-group mb-0">
+                                    <select class="selectpicker" id="type" name="type" style="display: none;">
+                                        <option value="1">商品</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group mb-0">
+                        <div class="modal-footer pb-r">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+                            <button type="submit" class="btn btn-magenta">确定</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
