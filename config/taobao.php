64662960c5a9cb00eb3ce366ec82d934f8454f09
liliangheng <liheng666@foxmail.com>
2016-11-21 19:04:49 +0800

    change

---
+++ config/taobao.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 淘宝平台sdk配置
+ */
+return [
+    'appKey' => '23538050',
+    'secretKey' => '9c073ac2309248aabbcdd0e97b4bca90',
+
+    /**
+     * 请求地址
+     */
+    'gatewayUrl' => 'http://gw.api.tbsandbox.com/router/rest',
+
+    /**
+     * 数据格式
+     */
+    'format' => 'json',
+];
