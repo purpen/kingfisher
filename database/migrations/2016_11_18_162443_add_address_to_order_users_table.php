6cc7b7939a6b3872146fd2e692b8eec6c4995824
purpen <purpen.w@gmail.com>
2016-12-13 23:02:16 +0800

    add union model

--- database/migrations/2016_11_18_162443_add_address_to_order_users_table.php
+++ database/migrations/2016_11_18_162443_add_address_to_order_users_table.php
@@ -12,7 +12,7 @@ class AddAddressToOrderUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('order_users', function (Blueprint $table) {
+        Schema::table('membership', function (Blueprint $table) {
             $table->string('buyer_address',200)->nullable();
             $table->string('buyer_province',20)->nullable();
             $table->string('buyer_city',20)->nullable();
@@ -27,7 +27,7 @@ class AddAddressToOrderUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('order_users', function (Blueprint $table) {
+        Schema::table('membership', function (Blueprint $table) {
             $table->dropColumn(['buyer_address' , 'buyer_province' , 'buyer_city' , 'buyer_county']);
         });
     }
