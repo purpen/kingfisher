4b39696012b8649f498a51ea1cbef1af6de46c2c
purpen <purpen.w@gmail.com>
2016-12-23 18:11:39 +0800

    debug file uplaod

--- app/Http/Controllers/Common/AssetController.php
+++ app/Http/Controllers/Common/AssetController.php
@@ -57,7 +57,7 @@ class AssetController extends Controller
                     'key' => $asset->path,
                     'payload' => [
                         'success' => 1,
-                        'name' => config('qiniu.url').$asset->path,
+                        'name' => config('qiniu.url').$asset->path.config('qiniu.small'),
                         'small' => config('qiniu.url').$asset->path.config('qiniu.small'),
                         'asset_id' => $id
                     ]
