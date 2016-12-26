ac89665874ede0d2f5545940362e820318c6f24a
clg <731992627@qq.com>
2016-12-26 17:19:37 +0800

    update会员

---
+++ database/migrations/2016_12_26_135330_update_order_users_to_membership_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateOrderUsersToMembershipTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::rename('order_users', 'membership');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('order_users');
+    }
+}
