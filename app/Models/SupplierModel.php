4864c5b2b3323f90b77dee368522fadc01a06927
purpen <purpen.w@gmail.com>
2017-01-05 19:26:37 +0800

    update purchase

--- app/Models/SupplierModel.php
+++ app/Models/SupplierModel.php
@@ -28,7 +28,7 @@ class SupplierModel extends BaseModel
 
     //供应商列表
     public function lists(){
-        $suppliers = self::where('status',2)->select('id','name')->get();
+        $suppliers = self::where('status',2)->select('id','name','nam')->get();
         return $suppliers;
     }
 
