47c38b9e41dedc9470fe8de70570dc562a5a5bdc
llh <liheng666@foxmail.com>
2017-01-09 20:36:26 +0800

    change

--- app/Models/PaymentOrderModel.php
+++ app/Models/PaymentOrderModel.php
@@ -168,7 +168,7 @@ class PaymentOrderModel extends BaseModel
     }
 
     /**
-     * 生成关联收款单号,ID
+     * 生成关联收款单
      */
     public function getReceiveOrderAttribute()
     {
--- resources/views/home/payment/completePayment.blade.php
+++ resources/views/home/payment/completePayment.blade.php
@@ -36,6 +36,7 @@
                         <th>应付金额</th>
                         <th>收支类型</th>
                         <th>相关单据</th>
+                        <th>收款单号</th>
                         <th>备注</th>
                         <th>创建人</th>
                         <th>创建时间</th>
@@ -51,11 +52,15 @@
                             <td>{{$v->amount}}</td>
                             <td>@if($v->purchase)【{{$v->purchase->supplier_type_val}}】@endif{{$v->type_val}}</td>
                             <td>{{$v->target_number}}</td>
+                            <td>@if($v->receive_order) <a target="_blank" href="{{ url('/receive/detailedReceive') }}?id={{$v->receive_order->id}}">{{$v->receive_order->number}}</a> @endif</td>
                             <td>{{$v->summary}}</td>
                             <td>@if($v->user){{$v->user->realname}} @else 自动同步 @endif</td>
                             <td>{{ $v->created_at_val }}</td>
                             <td>
                                 <a href="{{url('/payment/detailedPayment')}}?id={{$v->id}}" class="btn btn-white btn-sm mr-r">查看</a>
+                                @role(['admin'])
+                                <a href="{{url('/payment/editPayable')}}?id={{$v->id}}" class="magenta-color mr-r">编辑</a>
+                                @endrole
                             </td>
                         </tr>
                     @endforeach
