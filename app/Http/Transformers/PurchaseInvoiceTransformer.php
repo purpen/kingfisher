82fcf6e55365fc187fac8ef4e8093110f93823f9
Your Name <you@example.com>
2017-06-30 14:45:17 +0800

    change api caigou

--- app/Http/Transformers/PurchaseInvoiceTransformer.php
+++ app/Http/Transformers/PurchaseInvoiceTransformer.php
@@ -12,13 +12,14 @@ class PurchaseInvoiceTransformer extends TransformerAbstract
         return [
             'id' => $pInvoices->purchase_sku_id,
             'invoice_info' => $pInvoices->invoice_info,
-            'invoice_time' => $pInvoices->invoice_info ? $pInvoices->created_at : '',
+            'invoice_time' => $pInvoices->created_at,
             'price' => $pInvoices->price,
             'storage_status' => $pInvoices->storage_status,
             'mode' => $pInvoices->mode,
             'weight' => $pInvoices->weight,
             'quantity' => $pInvoices->count,
             'product_name' => $pInvoices->title,
+            'verified' => $pInvoices->verified,
 
         ];
     }
