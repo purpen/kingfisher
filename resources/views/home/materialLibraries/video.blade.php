aef5595c3b38f2cf1b953ce0ef8f83a9097775fa
Your Name <you@example.com>
2017-07-06 17:06:50 +0800

    change

--- resources/views/home/materialLibraries/video.blade.php
+++ resources/views/home/materialLibraries/video.blade.php
@@ -107,7 +107,7 @@
                         @foreach($materialLibraries as $materialLibrary)
                             <tr>
                                 <th>
-                                    <img src="{{$materialLibrary->first_img}}" class="img-thumbnail" style="width: 80px;">
+                                    <img src="{{ url('images/default/video.png') }}" class="img-thumbnail" style="width: 80px;">
                                 </th>
                                 <th>{{ $product->title }}</th>
                                 <th>{{ $materialLibrary->product_number }}</th>
