df2b0107aaabd633ef02a7e8be0085578eea831b
Your Name <you@example.com>
2017-07-14 15:14:40 +0800

    change

--- app/Http/Controllers/Api/SaasV1/MaterialLibrariesController.php
+++ app/Http/Controllers/Api/SaasV1/MaterialLibrariesController.php
@@ -465,7 +465,8 @@ class MaterialLibrariesController extends BaseController
 //        $article->article_type = 2;
 //        $content = $request['content'];
 //        Log::info($request->getContent());
-        Log::info(json_encode($request->all()));
+        Log::info($GLOBALS['HTTP_RAW_POST_DATA']);
+//        Log::info(json_encode($request));
     }
     /**
      * Show the form for creating a new resource.
