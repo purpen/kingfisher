69ad3c6529e60f9accc939896779ff70254ef919
llh <liheng666@foxmail.com>
2017-07-20 10:16:39 +0800

    change

--- app/Http/SaasTransformers/UserTransformer.php
+++ app/Http/SaasTransformers/UserTransformer.php
@@ -20,7 +20,7 @@ class UserTransformer extends TransformerAbstract
 //            'email' => $user->email,
             'phone' => $user->phone,
 //            'realname' => $user->realname,
-            'cover' => $user->cover ? $user->cover->file : null,
+            'cover' => $user->cover_id ? $user->cover->file : null,
         ];
     }
 }
