5fb0ab3251374cf5e958d43244f81d7442b79605
Your Name <you@example.com>
2017-08-01 11:39:01 +0800

    change

--- app/Http/SaasTransformers/VideoTransformer.php
+++ app/Http/SaasTransformers/VideoTransformer.php
@@ -14,8 +14,6 @@ class VideoTransformer extends TransformerAbstract
             'product_number' => $videos->product_number,
             'describe' => $videos->describe,
             'asset' => $videos->file,
-            'video' => $videos->file->srcfile,
-            'video_image' => $videos->file->video,
             'video_size' => $videos->size,
             'video_created' => $videos->created_at,
             'product' => $videos->product,
