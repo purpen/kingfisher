5cd62ee57ec75e559cb930136a4fc8f3bc431c52
Your Name <you@example.com>
2017-08-02 11:40:48 +0800

    change

--- app/Http/Controllers/Api/SaasV1/PosterController.php
+++ app/Http/Controllers/Api/SaasV1/PosterController.php
@@ -13,6 +13,153 @@ class PosterController extends BaseController
      * @apiGroup Poster
      *
      * @apiParam {string} token token
+     *
+     * @apiSuccessExample 成功响应:
+    {
+        "meta": {
+            "message": "Success",
+            "status_code": 200
+        },
+        "data": {
+            "id": 1,
+            "background_color": "#FFFFFF",
+            "size": {
+                "width": 750,
+                "height": 1334
+            },
+            "image": [
+                {
+                    "height": 1334,
+                    "zindex": 11,
+                    "width": 750,
+                    "type": 97,
+                    "name": "photo",
+                    "imageUrl": "",
+                    "position": {
+                        "right": 0,
+                        "top": 0,
+                        "left": 0,
+                        "bottom": 0
+                    }
+                },
+                {
+                    "height": 150,
+                    "zindex": 9,
+                    "width": 150,
+                    "type": 99,
+                    "name": "qr",
+                    "imageUrl": "",
+                    "position": {
+                        "right": -210,
+                        "top": 1084,
+                        "left": 390,
+                        "bottom": 100
+                    }
+                },
+                {
+                    "height": 150,
+                    "zindex": 10,
+                    "width": 150,
+                    "type": 98,
+                    "name": "logo",
+                    "imageUrl": "",
+                        "position": {
+                        "right": -390,
+                        "top": 1084,
+                        "left": 210,
+                        "bottom": -100
+                        }
+                }
+            ],
+            "text": [
+            {
+                "fontSize": 72,
+                "color": "#FFFFFF",
+                "content": "2017 HOOEASY",
+                "height": 100,
+                "weight": 4,
+                "width": 610,
+                "zindex": 1,
+                "align": 1,
+                "background": "#",
+                "position": {
+                    "right": -70,
+                    "top": 117,
+                    "left": 70,
+                    "bottom": -1117
+                }
+            },
+            {
+                "fontSize": 58,
+                "color": "#FFFFFF",
+                "content": "NEW PEODUCT",
+                "height": 81,
+                "weight": 4,
+                "width": 610,
+                "zindex": 2,
+                "align": 1,
+                "background": "#",
+                "position": {
+                    "right": -70,
+                    "top": 196,
+                    "left": 70,
+                    "bottom": -1057
+                }
+            },
+            {
+                "fontSize": 48,
+                "color": "#FFFFFF",
+                "content": "LAUNCH德智衣护无界",
+                "height": 67,
+                "weight": 0,
+                "width": 610,
+                "zindex": 3,
+                "align": 1,
+                "background": "#",
+                "position": {
+                    "right": -70,
+                    "top": 264,
+                    "left": 70,
+                    "bottom": -1003
+                }
+            },
+            {
+                "fontSize": 74,
+                "color": "#FFFFFF",
+                "content": "好易点新品发布会",
+                "height": 67,
+                "weight": 0,
+                "width": 610,
+                "zindex": 5,
+                "align": 1,
+                "background": "#",
+                "position": {
+                    "right": -70,
+                    "top": 803,
+                    "left": 70,
+                    "bottom": -464
+                }
+            },
+            {
+                "fontSize": 40,
+                "color": "#FFFFFF",
+                "content": "诚邀莅临",
+                "height": 100,
+                "weight": 4,
+                "width": 610,
+                "zindex": 1,
+                "align": 1,
+                "background": "#",
+                    "position": {
+                        "right": -70,
+                        "top": 117,
+                        "left": 70,
+                        "bottom": -1117
+                    }
+                }
+            ]
+        }
+    }
      */
     public function lists()
     {
@@ -66,6 +213,152 @@ class PosterController extends BaseController
      * @apiGroup Poster
      *
      * @apiParam {string} token token
+     * @apiSuccessExample 成功响应:
+    {
+        "meta": {
+            "message": "Success",
+            "status_code": 200
+        },
+        "data": {
+            "id": 1,
+            "background_color": "#FFFFFF",
+            "size": {
+                "width": 750,
+                "height": 1334
+            },
+            "image": [
+                {
+                    "height": 1334,
+                    "zindex": 11,
+                    "width": 750,
+                    "type": 97,
+                    "name": "photo",
+                    "imageUrl": "",
+                    "position": {
+                        "right": 0,
+                        "top": 0,
+                        "left": 0,
+                        "bottom": 0
+                    }
+                },
+                {
+                    "height": 150,
+                    "zindex": 9,
+                    "width": 150,
+                    "type": 99,
+                    "name": "qr",
+                    "imageUrl": "",
+                    "position": {
+                        "right": -210,
+                        "top": 1084,
+                        "left": 390,
+                        "bottom": 100
+                    }
+                },
+                {
+                    "height": 150,
+                    "zindex": 10,
+                    "width": 150,
+                    "type": 98,
+                    "name": "logo",
+                    "imageUrl": "",
+                    "position": {
+                        "right": -390,
+                        "top": 1084,
+                        "left": 210,
+                        "bottom": -100
+                    }
+                }
+            ],
+            "text": [
+                {
+                    "fontSize": 72,
+                    "color": "#FFFFFF",
+                    "content": "2017 HOOEASY",
+                    "height": 100,
+                    "weight": 4,
+                    "width": 610,
+                    "zindex": 1,
+                    "align": 1,
+                    "background": "#",
+                    "position": {
+                        "right": -70,
+                        "top": 117,
+                        "left": 70,
+                        "bottom": -1117
+                    }
+                },
+                {
+                    "fontSize": 58,
+                    "color": "#FFFFFF",
+                    "content": "NEW PEODUCT",
+                    "height": 81,
+                    "weight": 4,
+                    "width": 610,
+                    "zindex": 2,
+                    "align": 1,
+                    "background": "#",
+                    "position": {
+                        "right": -70,
+                        "top": 196,
+                        "left": 70,
+                        "bottom": -1057
+                    }
+                },
+                {
+                    "fontSize": 48,
+                    "color": "#FFFFFF",
+                    "content": "LAUNCH德智衣护无界",
+                    "height": 67,
+                    "weight": 0,
+                    "width": 610,
+                    "zindex": 3,
+                    "align": 1,
+                    "background": "#",
+                    "position": {
+                        "right": -70,
+                        "top": 264,
+                        "left": 70,
+                        "bottom": -1003
+                    }
+                },
+                {
+                    "fontSize": 74,
+                    "color": "#FFFFFF",
+                    "content": "好易点新品发布会",
+                    "height": 67,
+                    "weight": 0,
+                    "width": 610,
+                    "zindex": 5,
+                    "align": 1,
+                    "background": "#",
+                    "position": {
+                        "right": -70,
+                        "top": 803,
+                        "left": 70,
+                        "bottom": -464
+                    }
+                },
+                {
+                    "fontSize": 40,
+                    "color": "#FFFFFF",
+                    "content": "诚邀莅临",
+                    "height": 100,
+                    "weight": 4,
+                    "width": 610,
+                    "zindex": 1,
+                    "align": 1,
+                    "background": "#",
+                    "position": {
+                        "right": -70,
+                        "top": 117,
+                        "left": 70,
+                        "bottom": -1117
+                    }
+                }
+            ]
+        }
+    }
      */
     public function poster()
     {
