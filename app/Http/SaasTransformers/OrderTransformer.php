b3e02d91949e7941f39c2342747f460f0699161f
Your Name <you@example.com>
2017-08-21 15:51:27 +0800

    add 优惠金额

--- app/Http/SaasTransformers/OrderTransformer.php
+++ app/Http/SaasTransformers/OrderTransformer.php
@@ -34,6 +34,7 @@ class OrderTransformer extends TransformerAbstract
             'user_id_sales' => $orders->user_id_sales,
 //            'storage_id' => $orders->storage_id,
             'payment_type' => $orders->payment_type,
+            'discount_money' => $orders->discount_money,
             'type' => $orders->type,
             'type_val' => $orders->type_val,
             'orderSku' => $orders->order_skus,
