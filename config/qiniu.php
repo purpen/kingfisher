455df5a69c197f2318ee597b78cddb27f058d5fd
Your Name <you@example.com>
2017-11-15 17:34:00 +0800

    add qiniu http https

--- config/qiniu.php
+++ config/qiniu.php
@@ -17,10 +17,10 @@ return [
     'url' => env('QINIU_URL', 'https://kg.erp.taihuoniao.com/'),                    //图片服务器
     'small' => '-sm',              //缩略图
 
-    'material_call_back_url' => 'http://k.taihuoniao.com/material/callback',  //测试服务器回调url
-    'material_persistent_url' => 'http://k.taihuoniao.com/material/qiniuNotify',
-    'material_upload_url' => 'http://up-z1.qiniu.com',
+    'material_call_back_url' => env('QINIU_MATERIAL_CALL_BACK_URL' , 'https://erp.taihuoniao.com/material/callback'),  //测试服务器回调url
+    'material_persistent_url' => env('QINIU_MATERIAL_PERSISTENT_URL' , 'https://erp.taihuoniao.com/material/qiniuNotify'),
+    'material_upload_url' => env('QINIU_MATERIAL_UPLOAD_URL' , 'https://up-z1.qiniu.com'),
     'material_bucket_name' => 'frmaterial', // 存储素材的空间
-    'material_url' => 'https://f3g.taihuoniao.com/', // 存储素材的图片服务器
+    'material_url' => env('MATERIAL_URL' , 'https://f3g.taihuoniao.com/'), // 存储素材的图片服务器
     'saas_domain' => 'saas_erp',                                              //存储域
 ];
