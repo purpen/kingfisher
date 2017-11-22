43350f87dc3b928354e3c3c8ab58146e1f99c55f
Your Name <you@example.com>
2017-11-22 11:15:51 +0800

    update code register

--- app/Http/Controllers/Api/MicroV1/AuthenticateController.php
+++ app/Http/Controllers/Api/MicroV1/AuthenticateController.php
@@ -199,10 +199,10 @@ class AuthenticateController extends BaseController
         $data['mobile'] = $credentials['account'];
         $data['text'] = '【太火鸟】验证码：' . $code . '，切勿泄露给他人，如非本人操作，建议及时修改账户密码。';
 
-//        $yunpian = new Yunpian();
-//        $yunpian->sendOneSms($data);
+        $yunpian = new Yunpian();
+        $yunpian->sendOneSms($data);
 
-        return $this->response->array(ApiHelper::success('请求成功！', 200 ,compact('code')));
+        return $this->response->array(ApiHelper::success('请求成功！', 200));
     }
 
 
