48661e2f790d7ff31129de5af438528a9a40b912
Your Name <you@example.com>
2017-12-04 17:35:15 +0800

    change

--- app/Http/Controllers/Api/MicroV1/PayController.php
+++ app/Http/Controllers/Api/MicroV1/PayController.php
@@ -66,9 +66,7 @@ class PayController extends BaseController
         $scope = "snsapi_base";
         $state = "STATE"."#wechat_redirect";
 
-        $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$appid.'&redirect_uri='.$redirect_uri.'&response_type='.$response_type.'&scope='.$scope.'&state='.$state;
-        Header("Location: $url");
-exit();
+        return "https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$appid.'&redirect_uri='.$redirect_uri.'&response_type='.$response_type.'&scope='.$scope.'&state='.$state;
     }
 
 }
