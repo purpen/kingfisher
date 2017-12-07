46d4487ab91eec05bf5eda2dded2dbb97a2b1396
adminlte <geforce96@hotmail.com>
2017-12-07 10:57:20 +0800

    add  choose language
    1. 把静态文字设置成变量

---
+++ wap-page/static/js/StaticData.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Administrator on 2017/12/6.
+ */
+export default {
+  chinaese: {
+    main: {
+      home: '首页', classify: '分类', cart: '购物车', mine: '我的'
+    },
+    home: {home: '首页'},
+    classify: {classify: '分类'},
+    cart: {cart: '购物车', edit: '编辑', checkout: '结算'},
+    i: {
+      introduce: '说说你是什么人，来自哪片山川湖海',
+      myorder: '我的订单',
+      seeAllorder: '查看全部订单',
+      mycoupon: '我的优惠券',
+      mymessage: '我的消息',
+      myaddr: '地址管理',
+      collect: '收藏',
+      track: '足迹',
+      waitPay: '待付款'，
+    },
+    addrControl: {
+      default: '默认地址',
+      setDefault: '设置默认地址',
+      edit: '编辑',
+      del: '删除',
+      add: '添加地址'
+    }
+  }
+}
