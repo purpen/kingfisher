0d649d42658b8a238e0d5ef45f8be36fa8aeda0e
adminlte <geforce96@hotmail.com>
2017-12-11 13:37:01 +0800

    add pay transfer

---
+++ wap-page/src/components/page/product/order/payTransfer.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+<script>
+  //  import api from '@/api/api'
+  export default {
+    name: '',
+    data () {
+      return {
+        orderid: 0,
+        code: ''
+      }
+    },
+    created () {
+      console.log(this.$route)
+    }
+  }
+</script>
+<style scoped>
+
+</style>
