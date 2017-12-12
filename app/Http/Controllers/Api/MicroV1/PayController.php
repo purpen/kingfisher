7c6804892bf7aea243f6b1427de24a09b9a5d6d0
Your Name <you@example.com>
2017-12-12 17:43:10 +0800

    change

--- app/Http/Controllers/Api/MicroV1/PayController.php
+++ app/Http/Controllers/Api/MicroV1/PayController.php
@@ -47,11 +47,10 @@ class PayController extends BaseController
         $jsApiParameter = $WxPay->wxPayApi($code , 'Micro商城订单' , $total*100 , $pay_order->uid);
         //获取签名
         $jsApiParameters = json_decode($jsApiParameter , true);
-Log::info(config('wxpay.redirect_code_url').'?'.$_SERVER['QUERY_STRING']);
         Log::info(Redis::get('wx_ticket'));
         Log::info($jsApiParameters['nonceStr']);
         Log::info($jsApiParameters['timeStamp']);
-        $signature = MD5('jsapi_ticket='.Redis::get('wx_ticket').'&noncestr='.$jsApiParameters['nonceStr'].'&timestamp='.$jsApiParameters['timeStamp'].'&url='.config('wxpay.redirect_code_url').'?'.$_SERVER['QUERY_STRING']);
+        $signature = sha1('jsapi_ticket='.Redis::get('wx_ticket').'&noncestr='.$jsApiParameters['nonceStr'].'&timestamp='.$jsApiParameters['timeStamp'].'&url=http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].$_SERVER['QUERY_STRING']);
 
         $jsApiParameters['signature'] = $signature;
         Log::info($jsApiParameters);
