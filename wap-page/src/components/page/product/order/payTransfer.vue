a89aea1c2326872cb9bf4253ca5a3e8e0ed11357
adminlte <geforce96@hotmail.com>
2017-12-12 13:45:17 +0800

    恢复到11点

--- wap-page/src/components/page/product/order/payTransfer.vue
+++ wap-page/src/components/page/product/order/payTransfer.vue
@@ -23,50 +23,52 @@
           }
         }).then((res) => {
           console.log(res)
-//           if (res.data.meta.status_code === 200) {
-//             wx.config({
-//               appId: config.appId,
-//               timestamp: config.timeStamp,
-//               nonceStr: config.nonceStr,
-// //              signature
-//               jsApiList: ['chooseWXPay']
-//             })
-//             wx.ready(() => {
-//               wx.chooseWXPay({
-//                 appId: config.appId,
-//                 nonceStr: config.nonceStr,
-//                 package: config.package,
-//                 timeStamp: config.timeStamp,
-//                 signType: config.signType,
-//                 paySign: config.paySign,
-//                 success (r) {
-//                   if (r.errMsg === 'chooseWXPay:ok') {
-//                     window.alert('支付成功')
-//                     window.location.reload()
-//                   } else {
-//                     window.alert(' 支付失败')
-//                     window.location.reload()
-//                   }
-//                 },
-//                 cancel () {
-//                   window.alert('支付取消')
-//                   window.location.reload()
-//                 },
-//                 error () {
-//                   window.alert('支付失败')
-//                   window.location.reload()
-//                 }
-//               })
-//             })
-//           } else {
-//             this.$message.error(res.data.meta.message)
-//           }
+          //           if (res.data.meta.status_code === 200) {
+          //             wx.config({
+          //               appId: config.appId,
+          //               timestamp: config.timeStamp,
+          //               nonceStr: config.nonceStr,
+          // //              signature
+          //               jsApiList: ['chooseWXPay']
+          //             })
+          //             wx.ready(() => {
+          //               wx.chooseWXPay({
+          //                 appId: config.appId,
+          //                 nonceStr: config.nonceStr,
+          //                 package: config.package,
+          //                 timeStamp: config.timeStamp,
+          //                 signType: config.signType,
+          //                 paySign: config.paySign,
+          //                 success (r) {
+          //                   if (r.errMsg === 'chooseWXPay:ok') {
+          //                     window.alert('支付成功')
+          //                     window.location.reload()
+          //                   } else {
+          //                     window.alert(' 支付失败')
+          //                     window.location.reload()
+          //                   }
+          //                 },
+          //                 cancel () {
+          //                   window.alert('支付取消')
+          //                   window.location.reload()
+          //                 },
+          //                 error () {
+          //                   window.alert('支付失败')
+          //                   window.location.reload()
+          //                 }
+          //               })
+          //             })
+          //           } else {
+          //             this.$message.error(res.data.meta.message)
+          //           }
         }).catch((err) => {
           console.error(err)
         })
       }
     },
     created () {
+      console.log(this)
+      console.log(this.$route)
       this.code = this.$route.query.code
       this.order_id = this.$route.query.order_id
       this.token = this.$route.query.token
