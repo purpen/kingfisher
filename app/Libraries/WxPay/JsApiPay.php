2dabd68492afd51123ead97a9f202ec886509b1c
Your Name <you@example.com>
2017-12-12 19:56:30 +0800

    change

--- app/Libraries/WxPay/JsApiPay.php
+++ app/Libraries/WxPay/JsApiPay.php
@@ -73,7 +73,6 @@ class JsApiPay
 	 */
 	public function GetJsApiParameters($UnifiedOrderResult)
 	{
-	    Log::info($UnifiedOrderResult['prepay_id']);
 		if(!array_key_exists("appid", $UnifiedOrderResult)
 		|| !array_key_exists("prepay_id", $UnifiedOrderResult)
 		|| $UnifiedOrderResult['prepay_id'] == "")
