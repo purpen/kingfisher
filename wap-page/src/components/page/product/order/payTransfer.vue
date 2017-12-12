03c7856baf06019d65cfcc57aea2b093b4f01282
adminlte <geforce96@hotmail.com>
2017-12-12 15:30:14 +0800

    添加ge/product/order/payTransfer.vue.

--- wap-page/src/components/page/product/order/payTransfer.vue
+++ wap-page/src/components/page/product/order/payTransfer.vue
@@ -14,7 +14,7 @@
     },
     methods: {
       choosewxPay () {
-        // let wx = require('weixin-js-sdk')
+        let wx = require('weixin-js-sdk')
         this.$http.get(api.wxPay, {
           params: {
             order_id: this.order_id,
@@ -22,15 +22,51 @@
             token: this.token
           }
         }).then((res) => {
-          console.log(res)
+          if (res.data.meta.status_code === 200) {
+            let config = res.data.data.jsApiParameters
+            wx.config({
+              appId: config.appId,
+              timestamp: config.timeStamp,
+              nonceStr: config.nonceStr,
+              signature: config.signature,
+              jsApiList: ['chooseWXPay']
+            })
+            wx.ready(() => {
+              wx.chooseWXPay({
+                appId: config.appId,
+                nonceStr: config.nonceStr,
+                package: config.package,
+                timeStamp: config.timeStamp,
+                signType: config.signType,
+                paySign: config.paySign,
+                success (r) {
+                  if (r.errMsg === 'chooseWXPay:ok') {
+                    window.alert('支付成功')
+                    window.location.reload()
+                  } else {
+                    window.alert(' 支付失败')
+                    window.location.reload()
+                  }
+                },
+                cancel () {
+                  window.alert('支付取消')
+                  window.location.reload()
+                },
+                error () {
+                  window.alert('支付失败')
+                  window.location.reload()
+                }
+              })
+            })
+          } else {
+            this.$message.error(res.data.meta.message)
+          }
         }).catch((err) => {
           console.error(err)
         })
       }
     },
     created () {
-      console.log(this)
-      console.log(this.$route)
       this.code = this.$route.query.code
       this.order_id = this.$route.query.order_id
       this.token = this.$route.query.token
