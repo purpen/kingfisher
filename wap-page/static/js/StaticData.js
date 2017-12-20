9f8174648d293e7c567020aee0ec48a8e5e9b2e2
LiZeHua <geforce96@hotmail.com>
2017-12-21 00:40:21 +0800

    本地化

--- wap-page/static/js/StaticData.js
+++ wap-page/static/js/StaticData.js
@@ -2,13 +2,13 @@
  * Created by Administrator on 2017/12/6.
  */
 export default {
-  chinaese: {
+  chinese: {
     main: {
       home: '首页', classify: '分类', cart: '购物车', mine: '我的'
     },
-    home: {home: '首页'},
-    classify: {classify: '分类'},
-    cart: {cart: '购物车', edit: '编辑', checkout: '结算'},
+    home: { home: '首页' },
+    classify: { classify: '分类' },
+    cart: { cart: '购物车', edit: '编辑', checkout: '结算' },
     i: {
       introduce: '说说你是什么人，来自哪片山川湖海',
       myorder: '我的订单',
@@ -18,7 +18,11 @@ export default {
       myaddr: '地址管理',
       collect: '收藏',
       track: '足迹',
-      waitPay: '待付款'
+      waitPay: '待付款',
+      waitSend: '待发货',
+      waitConfirm: '待收货',
+      waitRate: '待评价',
+      afterMarket: '退款/售后'
     },
     addrControl: {
       default: '默认地址',
@@ -26,6 +30,36 @@ export default {
       edit: '编辑',
       del: '删除',
       add: '添加地址'
+    }
+  },
+  english: {
+    main: {
+      home: 'home', classify: 'classify', cart: 'cart', mine: 'mine'
+    },
+    home: { home: 'home' },
+    classify: { classify: 'classify' },
+    cart: { cart: 'cart', edit: 'edit', checkout: 'checkout' },
+    i: {
+      introduce: 'Introduce yourself',
+      myorder: 'myOrder',
+      seeAllorder: 'seeAllorder',
+      mycoupon: 'myCoupon',
+      mymessage: 'myMessage',
+      myaddr: 'myAddress',
+      collect: 'collect',
+      track: 'track',
+      waitPay: 'waitPay',
+      waitSend: 'waitSend',
+      waitConfirm: 'waitConfirm',
+      waitRate: 'waitRate',
+      afterMarket: 'afterMarket'
+    },
+    addrControl: {
+      default: 'defaultAddress',
+      setDefault: 'setDefault',
+      edit: 'Edit',
+      del: 'Del',
+      add: 'Add'
     }
   }
 }
