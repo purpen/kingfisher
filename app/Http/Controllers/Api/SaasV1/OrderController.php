e21790186804250332a33d2a58d71762681bf5b9
CaiLiguang <731994627@qq.com>
2018-05-04 16:33:26 +0800

    x修改供应商订单列表顺序

--- app/Http/Controllers/Api/SaasV1/OrderController.php
+++ app/Http/Controllers/Api/SaasV1/OrderController.php
@@ -635,7 +635,7 @@ class OrderController extends BaseController
                 ->join('logistics', 'order.express_id', '=', 'logistics.id')
                 ->whereBetween('order_sku_relation.created_at', [$start_date, $end_date])
                 ->where('products.supplier_id', '=', $supplier->id)
-                ->where('order.status', '=', $status)->paginate($per_page);;
+                ->where('order.status', '=', $status)->orderBy('order.id', 'desc')->paginate($per_page);;
         }else{
             $orders = DB::table('order_sku_relation')
                 ->join('products', 'products.id', '=', 'order_sku_relation.product_id')
@@ -643,7 +643,7 @@ class OrderController extends BaseController
                 ->join('products_sku', 'order_sku_relation.sku_id', '=', 'products_sku.id')
                 ->join('logistics', 'order.express_id', '=', 'logistics.id')
                 ->whereBetween('order_sku_relation.created_at', [$start_date, $end_date])
-                ->where('products.supplier_id', '=', $supplier->id)->paginate($per_page);
+                ->where('products.supplier_id', '=', $supplier->id)->orderBy('order.id', 'desc')->paginate($per_page);
         }
         return $this->response->paginator($orders, new SupplierOrderTransformer())->setMeta(ApiHelper::meta());
 
