a1c98c811e5f711c163861bd5e62344d16d26237
durong <282235309@qq.com>
2018-05-12 00:10:51 +0800

    新增允许批量赋值字段

--- app/Models/SupplierModel.php
+++ app/Models/SupplierModel.php
@@ -27,7 +27,7 @@ class SupplierModel extends BaseModel
      * 允许批量赋值的字段
      */
 //    protected  $fillable = ['name','address','legal_person','tel','ein','bank_number','bank_address','general_taxpayer','contact_user','contact_number','contact_email','contact_qq','contact_wx','summary','cover_id','discount','tax_rate','type','nam','start_time','end_time','relation_user_id' , 'random_id','msg','status'];
-    protected  $fillable = ['name','address','legal_person','tel','ein','bank_number','bank_address','general_taxpayer','contact_user','contact_number','contact_email','contact_qq','contact_wx','summary','cover_id','discount','tax_rate','type','nam','start_time','end_time','relation_user_id' , 'random_id','mould_id' , 'trademark_id' , 'power_of_attorney_id' , 'quality_inspection_report_id' , 'authorization_deadline' , 'supplier_user_id'];
+    protected  $fillable = ['name','address','legal_person','tel','ein','bank_number','bank_address','general_taxpayer','contact_user','contact_number','contact_email','contact_qq','contact_wx','summary','cover_id','discount','tax_rate','type','nam','start_time','end_time','relation_user_id' , 'random_id','mould_id' , 'trademark_id' , 'power_of_attorney_id' , 'quality_inspection_report_id' , 'authorization_deadline' , 'supplier_user_id','msg','status'];
 
     //供应商列表
     public function lists(){
