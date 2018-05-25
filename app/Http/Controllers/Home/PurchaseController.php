782d0e9ab5eb60374095b2d3c692f9acc1a11cbe
durong <282235309@qq.com>
2018-05-25 14:23:30 +0800

    add 付款条件

--- app/Http/Controllers/Home/PurchaseController.php
+++ app/Http/Controllers/Home/PurchaseController.php
@@ -406,6 +406,7 @@ class PurchaseController extends Controller
             $purchase->surcharge = $surcharge;
             $purchase->user_id = Auth::user()->id;
             $purchase->invoice_info = $invoice_info;
+            $purchase->paymentcondition = $paymentcondition;
 
             if ($purchase->save()) {
                 DB::table('purchase_sku_relation')->where('purchase_id', $purchase_id)->delete();
