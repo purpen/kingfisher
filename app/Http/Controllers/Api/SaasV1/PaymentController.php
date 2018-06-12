9f7274bc941ecbc55f5caf62ccf435c359937aa0
durong <282235309@qq.com>
2018-06-12 13:55:58 +0800

    添加注释

--- app/Http/Controllers/Api/SaasV1/PaymentController.php
+++ app/Http/Controllers/Api/SaasV1/PaymentController.php
@@ -8,9 +8,47 @@ use Illuminate\Http\Request;
 
 class PaymentController extends BaseController{
 
+      /**
+       * @api {get} /saasApi/payment 品牌付款单列表
+       * @apiVersion 1.0.0
+       * @apiName Payment payment
+       * @apiGroup Payment
+       *
+       * @apiParam {integer} status 状态: 0.默认 1.待采购确认 2.待供应商确认 3.待确认付款 4.完成
+       * @apiParam {string} token token
+       * @apiSuccessExample 成功响应:
+      {
+      "data": [
+      {
+      "id": 2,
+      "number": "PP2018061100012",  //单号
+      "supplier_user_id": "魅族科技",  //供货商ID
+      "start_time": "0000-00-00 00:00:00",  //开始时间
+      "end_time": "0000-00-00 00:00:00",  //结束时间
+      "total_price": 1000,  //总金额
+      "status": 2,  //状态: 0.默认 1.待采购确认 2.待供应商确认 3.待确认付款 4.完成
+      },
+      ],
+      "meta": {
+      "message": "Success.",
+      "status_code": 200,
+      "pagination": {
+      "total": 1,
+      "count": 1,
+      "per_page": 10,
+      "current_page": 1,
+      "total_pages": 1,
+      "links": {
+      "next": "http://www.work.com/saasApi/payment?page=2"
+      }
+      }
+      }
+      }
+       *
+       *
+       */
 //    品牌付款单列表
     public function payment(Request $request){
-        echo 111;die;
         $status = (int)$request->input('status', 0);
         $per_page = (int)$request->input('per_page', 10);
         $supplier_user_id = $request->input('supplier_user_id');
