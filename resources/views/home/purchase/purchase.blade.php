c4eefb306d1d9835337256469fa25291e5d74440
durong <282235309@qq.com>
2018-06-22 16:19:04 +0800

    修改js报错

--- resources/views/home/purchase/purchase.blade.php
+++ resources/views/home/purchase/purchase.blade.php
@@ -505,7 +505,7 @@
         </div>
     </div>
 
-    <script language="javascript" src="{{url('assets/Lodop/LodopFuncs.js')}}"></script>
+
     <object  id="LODOP_OB" classid="clsid:2105C259-1E0C-4534-8141-A753534CB4CA" width=0 height=0>
         <embed id="LODOP_EM" type="application/x-print-lodop" width=0 height=0></embed>
     </object>
@@ -514,6 +514,6 @@
 
     {{--导入弹出框--}}
     @include('home/purchase.inPurchase')
-
+    <script src="{{ url('build/assets/js/LodopFuncs.js') }}"></script>
 @endsection
 
