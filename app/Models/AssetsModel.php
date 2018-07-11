a8db44285a8c49243bf4585b634390f5bcb397c1
dr <2834144959@qq.com>
2018-07-11 18:25:31 +0800

    修改模型

--- app/Models/AssetsModel.php
+++ app/Models/AssetsModel.php
@@ -36,6 +36,15 @@ class AssetsModel extends BaseModel
     }
 
     /**
+     * 一对一关联distributors表
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function distributor()
+    {
+        return $this->hasOne('App\Models\DistributorsModel', 'cover_id');
+    }
+
+    /**
      * 一对一关联productSku表
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
