b4c610770f4e0c18b390d89bf6a3cbefec1b6e32
dr <2834144959@qq.com>
2018-07-17 15:26:12 +0800

    修改类型

--- database/migrations/2018_07_11_160921_create_distributors_table.php
+++ database/migrations/2018_07_11_160921_create_distributors_table.php
@@ -24,7 +24,7 @@ class CreateDistributorsTable extends Migration
             $table->string('business_license_number',20);  //营业执照号
             $table->string('taxpayer',20)->default(0);  //纳税人(一般/小规模等)
             $table->integer('area_id')->default(0); //地域分类id
-            $table->json('authorization_id'); //授权条件id
+            $table->string('authorization_id',50); //授权条件id(多个)
             $table->timestamps();
 
         });
