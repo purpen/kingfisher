c566dd192210823308b82b7b390ea8b349c624f7
dr <2834144959@qq.com>
2018-07-23 13:01:31 +0800

    修改类名

--- database/migrations/2018_07_20_161548_create_distributor_table.php
+++ database/migrations/2018_07_20_161548_create_distributor_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDistributorsTable extends Migration
+class CreateDistributorTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2018_07_20_173831_add_category_id_to_distributor_table.php
+++ database/migrations/2018_07_20_173831_add_category_id_to_distributor_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddCategoryIdToDistributorsTable extends Migration
+class AddCategoryIdToDistributorTable extends Migration
 {
     /**
      * Run the migrations.
