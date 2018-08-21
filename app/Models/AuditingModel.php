72e63a0af0c2263706320f489cf2eab883ca7499
dr <2834144959@qq.com>
2018-08-21 17:06:12 +0800

    新增模型方法

--- app/Models/AuditingModel.php
+++ app/Models/AuditingModel.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use App\Libraries\YunPianSdk\Yunpian;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -11,7 +12,7 @@ class AuditingModel extends BaseModel
     protected $dates = ['deleted_at'];
 
     /**
-     * 关联模型到收货地址表
+     * 关联模型到审核管理表
      * @var string
      */
     protected $table = 'auditing';
@@ -23,4 +24,22 @@ class AuditingModel extends BaseModel
      */
     protected $fillable = ['type','user_id','status'];
 
+//    批量发送短信
+    public function datas($type)
+    {
+        $id = AuditingModel::where('type',$type)->select('user_id')->first();
+        $user_id = explode(",",$id->user_id);
+        $phone = UserModel::whereIn('id',$user_id)->select('phone')->get();
+        $phones = $phone->toArray();
+
+        $newArr = array();
+        for ($i = 0, $len = count($phones); $i < $len; $i++) {
+            $newArr[] = $phones[$i]['phone'];
+        }
+        $data = array();
+        $data['mobile'] = implode($newArr, ',');
+        $data['text'] = '【太火鸟】您有需要审核的信息，请及时登录ERP后台处理。如已处理完成，请忽略此短信。';
+        $yunpian = new Yunpian();
+        $yunpian->sendManySms($data);
+    }
 }
