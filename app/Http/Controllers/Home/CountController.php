351c3af8907bebbe0f659e272a01aff1f879d916
dr <2834144959@qq.com>
2018-08-24 20:00:05 +0800

    统计

--- app/Http/Controllers/Home/CountController.php
+++ app/Http/Controllers/Home/CountController.php
@@ -2,11 +2,147 @@
 namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
+use App\Models\ChinaCityModel;
+use App\Models\DistributorModel;
+use App\Models\ProductsModel;
+use App\Models\ProductsSkuModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CountController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+//        $products = $request->input('products');
+//        $sku_id = $request->input('sku_id');
+        $sku_id = 22;
+        $start_time4 = "2018-08-20";
+        $end_time4 = "2018-08-22";
+//        $time_slot = $request->input('time_slot');//时间段跟下面的开始时间、结束时间每次只能选一个
+
+        $count_money = DB::table('order_sku_relation')
+            ->join('products', 'products.id', '=', 'order_sku_relation.product_id')
+            ->join('order', 'order.id', '=', 'order_sku_relation.order_id')
+            ->where('order_sku_relation.sku_id','=',$sku_id)
+            ->whereBetween('order.order_send_time', [$start_time4, $end_time4])
+            ->select('order_sku_relation.quantity', 'order_sku_relation.price')
+            ->get();
+
+        var_dump($count_money);die;
+        die;
         return view('home/count.count');
     }
+
+//    铟立方总收入
+    public function ingathering(Request $request)
+    {
+        //$start_time1 = $request->input('start_time1');
+        //$end_time1 = $request->input('end_time1');
+        $start_time1 = "2018-08-20";
+        $end_time1 = "2018-08-22";
+
+        $orders=DB::table('order_sku_relation')
+            ->join('order', 'order.id', '=', 'order_sku_relation.order_id')
+            ->where('order.type','=',8)
+            ->whereBetween('order.order_send_time', [$start_time1, $end_time1])
+            ->get();
+        if (count($orders)>0) {
+            $ingathering = [];
+            $order_send_time = [];
+            foreach ($orders as $k => $v) {
+                $ingathering[] = $v->quantity * $v->price;//收入
+                $order_send_time[] = $v->order_send_time;//发货时间
+            }
+            return ajax_json(1, 'ok', $ingathering);
+
+        }else{
+            return ajax_json(0, 'error', '该时间段暂无订单收入！');
+        }
+    }
+
+//    获取所有的商品名称
+    public function products()
+    {
+        $products = ProductsModel::where('status', 2)
+            ->orderBy('id', 'desc')
+            ->get();
+    }
+
+//    ajax获取所选商品下所有的sku
+    public function skus(Request $request)
+    {
+        //$product_id = (int)$request->input('product_id');
+        $product_id = 21;
+        $product_sku = ProductsSkuModel::where('product_id',$product_id)->get();//所有该商品下的sku
+
+//        $product_sku = DB::table('order_sku_relation')
+//            ->join('products', 'products.id', '=', 'order_sku_relation.product_id')
+//            ->where('products.id','=',$product_id)
+//            ->select('order_sku_relation.id','order_sku_relation.sku_name')
+//            ->get();//        sku_name
+    }
+
+//    获取所有门店所在省份
+    public function province()
+    {
+        $province_id = DistributorModel::where('status',2)->select('province_id')->get();
+        $province = [];
+        foreach ($province_id as $v){
+            $province[] = ChinaCityModel::where('oid',$v->province_id)->select('name')->first();
+        }
+        $provinces = array_unique($province);//去除重复的省份
+
+    }
+
+    //    获取所有省市下面的门店
+    public function store(Request $request)
+    {
+//        $province_id = $request->input('province_id');
+        $province_id = 27;
+
+//        $store_names = DistributorModel::where('province_id',$province_id)->select('store_name')->get();
+        $store_names = DistributorModel::where('province_id',$province_id)->get();
+//        foreach ($store_names as $v){
+//            $store_name[] = $v->store_name;
+//        }
+//        var_dump($store_names->toArray());
+    }
+
+
+
+//    根据sku跟时间算商品收入
+    public function commodityIncome(Request $request)
+    {
+        $products = $request->input('products');
+        $skus = $request->input('skus');
+        $time_slot = $request->input('time_slot');//时间段跟下面的开始时间、结束时间每次只能选一个
+        $start_time4 = $request->input('start_time4');
+        $end_time4 = $request->input('end_time4');
+
+        $count_money = DB::table('order_sku_relation')
+            ->join('products', 'products.id', '=', 'order_sku_relation.product_id')
+            ->join('order', 'order.id', '=', 'order_sku_relation.order_id')
+            ->where('order_sku_relation.id','=',$skus)
+            ->whereBetween('order.order_send_time', [$start_time4, $end_time4])
+            ->select('order_sku_relation.quantity', 'order_sku_relation.price')
+            ->get();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- resources/views/home/count/count.blade.php
+++ resources/views/home/count/count.blade.php
@@ -77,7 +77,7 @@
         </div>
     </div>
     <div class="container mainwrap">
-    <div class="row">
+
         <div class="col-md-2">
             <div class="panel-group table-responsive" role="tablist">
                 <div class="panel panel-primary leftMenu">
@@ -168,22 +168,68 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-7">
-            <select class="selectpicker" id="goods" name="goods" style="display: none;">
+
+        <div class="row1">
+            <div class="col-md-8" style="margin-left: 54px">
+            <h3>铟立方总收入趋势</h3>
+            </div>
+            <br>
+            <div class="col-md-7">
+                <label for="inputStartTime" class="col-sm-2 control-label" style="width: 90px">开始时间</label>
+                <div class="col-sm-3" style="width: 125px">
+                    <input type="text" class="form-control datetimepicker start1" name="start_time1" value="" placeholder="开始时间" required autocomplete="off">
+                </div>
+                @if ($errors->has('start_time1'))
+                    <span class="help-block">
+                                    <strong>{{ $errors->first('start_time1') }}</strong>
+                                </span>
+                @endif
+                <label for="inputEndTime" class="col-sm-2 control-label" style="width: 90px">结束时间</label>
+                <div class="col-sm-3" style="width: 125px;">
+                    <input type="text" class="form-control datetimepicker end1" name="end_time1" value="" placeholder="结束时间" required autocomplete="off">
+                </div>
+                @if ($errors->has('end_time1'))
+                    <span class="help-block">
+                                    <strong>{{ $errors->first('end_time1') }}</strong>
+                                </span>
+                @endif
+        </div>
+
+            <div class="col-md-8">
+                <!-- 为 ECharts 准备一个具备大小（宽高）的 DOM -->
+                <div id="main1" style="width: 600px;height:400px;"></div>
+            </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+        <div class="row4" style="margin-left: 300px">
+        <div class="col-md-8" style="margin-left: 60px">
+            <select class="selectpicker" id="products" name="products" style="display: none;">
                 <option value="">请选择商品名称</option>
-                {{--@foreach($provinces as $v)--}}
-                <option value="1">订单审核</option>
+{{--                @foreach($products as $v)--}}
+                <option value="1">小米平衡车</option>
+{{--                <option value="{{$v->id}}">{{$v->title}}</option>--}}
                 {{--@endforeach--}}
             </select>
-            <select class="selectpicker" id="skus" name="skus" style="display: none;">
+            <select class="selectpicker" id="sku_id" name="sku_id" style="display: none;">
                 <option value="">请选择SKU</option>
-                {{--@foreach($provinces as $v)--}}
+                {{--@foreach($product_sku as $v)--}}
                 <option value="1">订单审核</option>
+{{--                <option value="{{$v->id}}">{{$v->mode}}</option>--}}
                 {{--@endforeach--}}
             </select>
             <br>
-            {{--<div class="col-md-8">--}}
-            <select class="selectpicker" id="times" name="times" style="display: none;">
+            <div class="col-md-8">
+            <select class="selectpicker" id="time_slot" name="time_slot" style="display: none;">
                 <option value="最近7天">最近7天</option>
                 <option value="最近30天">最近30天</option>
                 <option value="最近60天">最近60天</option>
@@ -191,54 +237,162 @@
             </select>
             <label for="inputStartTime" class="col-sm-2 control-label" style="width: 90px">开始时间</label>
             <div class="col-sm-3" style="width: 125px">
-                <input type="text" class="form-control datetimepicker start" name="start_times" value="" placeholder="开始时间" required autocomplete="off">
+                <input type="text" class="form-control datetimepicker start4" name="start_time4" value="" placeholder="开始时间" required autocomplete="off">
             </div>
-            @if ($errors->has('start_times'))
+            @if ($errors->has('start_time4'))
                 <span class="help-block">
-                                    <strong>{{ $errors->first('start_times') }}</strong>
+                                    <strong>{{ $errors->first('start_time4') }}</strong>
                                 </span>
             @endif
             <label for="inputEndTime" class="col-sm-2 control-label" style="width: 90px">结束时间</label>
             <div class="col-sm-3" style="width: 125px;">
-                <input type="text" class="form-control datetimepicker end" name="end_times" value="" placeholder="结束时间" required autocomplete="off">
+                <input type="text" class="form-control datetimepicker end4" name="end_time4" value="" placeholder="结束时间" required autocomplete="off">
             </div>
-            @if ($errors->has('end_times'))
+            @if ($errors->has('end_time4'))
                 <span class="help-block">
-                                    <strong>{{ $errors->first('end_times') }}</strong>
+                                    <strong>{{ $errors->first('end_time4') }}</strong>
                                 </span>
             @endif
-        {{--</div>--}}
+        </div>
         </div>
         <div class="col-md-8">
         <!-- 为 ECharts 准备一个具备大小（宽高）的 DOM -->
         <div id="main" style="width: 600px;height:400px;"></div>
         </div>
 
+        <br>
+        <div class="col-md-7">
         <label for="inputStartTime" class="col-sm-2 control-label" style="width: 90px">开始时间</label>
         <div class="col-sm-3" style="width: 125px">
-            <input type="text" class="form-control datetimepicker start1" name="start_time1" value="" placeholder="开始时间" required autocomplete="off">
+            <input type="text" class="form-control datetimepicker start5" name="start_time5" value="" placeholder="开始时间" required autocomplete="off">
         </div>
-        @if ($errors->has('start_time1'))
+        @if ($errors->has('start_time5'))
             <span class="help-block">
-                                    <strong>{{ $errors->first('start_time1') }}</strong>
+                                    <strong>{{ $errors->first('start_time5') }}</strong>
                                 </span>
         @endif
         <label for="inputEndTime" class="col-sm-2 control-label" style="width: 90px">结束时间</label>
         <div class="col-sm-3" style="width: 125px;">
-            <input type="text" class="form-control datetimepicker end1" name="end_time1" value="" placeholder="结束时间" required autocomplete="off">
+            <input type="text" class="form-control datetimepicker end5" name="end_time5" value="" placeholder="结束时间" required autocomplete="off">
         </div>
-        @if ($errors->has('end_time1'))
+        @if ($errors->has('end_time5'))
             <span class="help-block">
-                                    <strong>{{ $errors->first('end_time1') }}</strong>
+                                    <strong>{{ $errors->first('end_time5') }}</strong>
                                 </span>
         @endif
+
+            <button type="submit" id="out_purchase" class="btn btn-white mr-2r">
+                下载表格
+            </button>
+        </div>
+
+        </div>
+        <div class="col-md-7" style="margin-left: 300px">
+            <table class="table table-bordered table-striped">
+                <thead>
+                <tr class="gblack">
+                    <th>时间</th>
+                    <th>今日收入</th>
+                    <th>累积收入</th>
+                    <th>销售总量</th>
+                </tr>
+                </thead>
+                <tbody id="def">
+
+                </tbody>
+            </table>
+        </div>
+
+
+        {{--<div class="row5">--}}
+        {{--<div class="col-md-7" style="margin-left: 60px">--}}
+            {{--<select class="selectpicker" id="goods" name="goods" style="display: none;">--}}
+                {{--<option value="">请选择商品名称</option>--}}
+                {{--@foreach($provinces as $v)--}}
+                {{--<option value="1">订单审核</option>--}}
+                {{--@endforeach--}}
+            {{--</select>--}}
+            {{--<select class="selectpicker" id="skus" name="skus" style="display: none;">--}}
+                {{--<option value="">请选择SKU</option>--}}
+                {{--@foreach($provinces as $v)--}}
+                {{--<option value="1">订单审核</option>--}}
+                {{--@endforeach--}}
+            {{--</select>--}}
+            {{--<br>--}}
+            {{--<div class="col-md-8">--}}
+            {{--<select class="selectpicker" id="times" name="times" style="display: none;">--}}
+                {{--<option value="最近7天">最近7天</option>--}}
+                {{--<option value="最近30天">最近30天</option>--}}
+                {{--<option value="最近60天">最近60天</option>--}}
+                {{--<option value="最近90天">最近90天</option>--}}
+            {{--</select>--}}
+            {{--<label for="inputStartTime" class="col-sm-2 control-label" style="width: 90px">开始时间</label>--}}
+            {{--<div class="col-sm-3" style="width: 125px">--}}
+                {{--<input type="text" class="form-control datetimepicker start6" name="start_time6" value="" placeholder="开始时间" required autocomplete="off">--}}
+            {{--</div>--}}
+            {{--@if ($errors->has('start_time6'))--}}
+                {{--<span class="help-block">--}}
+                                    {{--<strong>{{ $errors->first('start_time6') }}</strong>--}}
+                                {{--</span>--}}
+            {{--@endif--}}
+            {{--<label for="inputEndTime" class="col-sm-2 control-label" style="width: 90px">结束时间</label>--}}
+            {{--<div class="col-sm-3" style="width: 125px;">--}}
+                {{--<input type="text" class="form-control datetimepicker end6" name="end_time6" value="" placeholder="结束时间" required autocomplete="off">--}}
+            {{--</div>--}}
+            {{--@if ($errors->has('end_time6'))--}}
+                {{--<span class="help-block">--}}
+                                    {{--<strong>{{ $errors->first('end_time6') }}</strong>--}}
+                                {{--</span>--}}
+            {{--@endif--}}
+        {{--</div>--}}
+        {{--</div>--}}
+        {{--<div class="col-md-8">--}}
+        {{--<!-- 为 ECharts 准备一个具备大小（宽高）的 DOM -->--}}
+        {{--<div id="main" style="width: 600px;height:400px;"></div>--}}
         {{--</div>--}}
 
-        <button type="submit" id="out_purchase" class="btn btn-white mr-2r">
-            下载表格
-        </button>
+        {{--<br>--}}
+        {{--<div class="col-md-7" style="margin-left: 300px">--}}
+        {{--<label for="inputStartTime" class="col-sm-2 control-label" style="width: 90px">开始时间</label>--}}
+        {{--<div class="col-sm-3" style="width: 125px">--}}
+            {{--<input type="text" class="form-control datetimepicker start7" name="start_time7" value="" placeholder="开始时间" required autocomplete="off">--}}
+        {{--</div>--}}
+        {{--@if ($errors->has('start_time7'))--}}
+            {{--<span class="help-block">--}}
+                                    {{--<strong>{{ $errors->first('start_time7') }}</strong>--}}
+                                {{--</span>--}}
+        {{--@endif--}}
+        {{--<label for="inputEndTime" class="col-sm-2 control-label" style="width: 90px">结束时间</label>--}}
+        {{--<div class="col-sm-3" style="width: 125px;">--}}
+            {{--<input type="text" class="form-control datetimepicker end7" name="end_time7" value="" placeholder="结束时间" required autocomplete="off">--}}
+        {{--</div>--}}
+        {{--@if ($errors->has('end_time7'))--}}
+            {{--<span class="help-block">--}}
+                                    {{--<strong>{{ $errors->first('end_time7') }}</strong>--}}
+                                {{--</span>--}}
+        {{--@endif--}}
+        {{--</div>--}}
 
-    </div>
+        {{--<button type="submit" id="out_purchase" class="btn btn-white mr-2r">--}}
+            {{--下载表格--}}
+        {{--</button>--}}
+        {{--</div>--}}
+        {{--<div class="col-md-7" style="margin-left: 300px">--}}
+            {{--<table class="table table-bordered table-striped">--}}
+                {{--<thead>--}}
+                {{--<tr class="gblack">--}}
+                    {{--<th>时间</th>--}}
+                    {{--<th>今日收入</th>--}}
+                    {{--<th>累积收入</th>--}}
+                    {{--<th>销售总量</th>--}}
+                {{--</tr>--}}
+                {{--</thead>--}}
+                {{--<tbody id="def">--}}
+
+                {{--</tbody>--}}
+            {{--</table>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 
     </div>
 
@@ -263,16 +417,23 @@
     $(this).find("a").toggleClass("glyphicon-chevron-up");
     $(this).find("a").toggleClass("glyphicon-chevron-show");
     });
-    });
 
+
+
+
+    {{--function(ec){--}}
+    {{--一级菜单对应的曲线图--}}
     {{--// 基于准备好的dom，初始化echarts实例--}}
-    var myChart = echarts.init(document.getElementById('main'));
+    var main = document.getElementById('main1');
+    var mainone = document.getElementById('main');
+    var charts = [];//同一个页面使用多个图表，把option实例存储在这个数组里
 
+    var mychart = echarts.init(main);
     {{--// 指定图表的配置项和数据--}}
     option = {
     xAxis: {
     type: 'category',
-    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    data: ['one', 'two', 'three', 'four', 'five', 'six', 'seven']
     },
     yAxis: {
     type: 'value'
@@ -285,7 +446,38 @@
     };
 
     {{--// 使用刚指定的配置项和数据显示图表。--}}
-    myChart.setOption(option);
+    mychart.setOption(option);
+    charts.push(mychart);
+
+    {{--二级菜单对应的曲线图--}}
+    mychart=echarts.init(mainone);
+    {{--// 指定图表的配置项和数据--}}
+    option = {
+    xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+    type: 'value'
+    },
+    series: [{
+    data: [1, 932, 901, 934, 1290, 1330, 2000],
+    type: 'line',
+    smooth: true
+    }]
+    };
+
+    {{--// 使用刚指定的配置项和数据显示图表。--}}
+    mychart.setOption(option);
+    charts.push(mychart);
+    window.onresize = function(){
+    for(var i = 0;i< charts.length;i++){
+        charts[i].resize();
+    }
+    }
+    });
+
+    {{--};--}}
 
 
 @endsection
