23560bdc73217f9e8f846e54b5333aa3bcc58abf
Unknown <3106800562@qq.com>
2018-08-28 11:48:42 +0800

    地址管理

---
+++ web-page/src/components/address-management/AddressManagementIndex.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+    export default {
+        name: '',
+        data () {
+            return {}
+        },
+        methods: {},
+        created: function () {
+
+        },
+        mounted () {
+
+        },
+        watch: {}
+    }
+</script>
