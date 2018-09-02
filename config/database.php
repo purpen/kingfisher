b2820ccb506ba7bb100329d1b7a293701612c521
liujiadong <1403417121@qq.com>
2018-09-02 14:20:27 +0800

    修改配置文件

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', 'Urk426#Db10'),
+            'database'  => env('DB_DATABASE', 'kingfisher'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => env('DB_PREFIX', 'laravel_'),
