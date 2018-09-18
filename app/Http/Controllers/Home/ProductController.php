03c38bf4827f364a3a6bd681740183db51dfa213
dr <2834144959@qq.com>
2018-09-18 11:38:02 +0800

    商品

--- app/Http/Controllers/Home/ProductController.php
+++ app/Http/Controllers/Home/ProductController.php
@@ -72,19 +72,19 @@ class ProductController extends Controller
         $name = '';
         // 分类列表
         $category = new CategoriesModel();
-        $lists = $category->lists(0,1);
+        $lists = $category->lists(0, 1);
 
-        if ($status === null){
-            $products = ProductsModel::orderBy('id','desc')->paginate($this->per_page);
-        }else{
-            $products = ProductsModel::where('status',$status)->orderBy('id','desc')->paginate($this->per_page);
+        if ($status === null) {
+            $products = ProductsModel::orderBy('id', 'desc')->paginate($this->per_page);
+        } else {
+            $products = ProductsModel::where('status', $status)->orderBy('id', 'desc')->paginate($this->per_page);
         }
 
-        $skus = ProductsSkuModel::orderBy('id','desc')->get();
+        $skus = ProductsSkuModel::orderBy('id', 'desc')->get();
         $suppliersModel = new SupplierModel();
         $suppliers = $suppliersModel->supplierList();
         $skuId = [];
-        foreach($skus as $sku){
+        foreach ($skus as $sku) {
             $skuId[] = $sku->product_id;
         }
         return view("home/product.home", [
@@ -103,9 +103,10 @@ class ProductController extends Controller
      * 获取唯一商品编码
      * @return int|string
      */
-    public function uniqueNumber(){
+    public function uniqueNumber()
+    {
         $number = getNumber();
-        if(ProductsModel::where('number',$number)->count() > 0){
+        if (ProductsModel::where('number', $number)->count() > 0) {
             $number = $this->uniqueNumber();
         }
         return $number;
@@ -136,14 +137,14 @@ class ProductController extends Controller
         $province = new ChinaCityModel();
         $provinces = $province->fetchCity();//所有省
 
-        return view('home/product.create',['number' => $number,'lists' => $lists,'random' => $random,'suppliers' => $suppliers,'user_id' => $user_id,'token' => $token, 'tab_menu' => $this->tab_menu,'name' => '', 'supplier_id' => 0,
-        'provinces'=>$provinces]);
+        return view('home/product.create', ['number' => $number, 'lists' => $lists, 'random' => $random, 'suppliers' => $suppliers, 'user_id' => $user_id, 'token' => $token, 'tab_menu' => $this->tab_menu, 'name' => '', 'supplier_id' => 0,
+            'provinces' => $provinces]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(ProductRequest $request)
@@ -157,39 +158,39 @@ class ProductController extends Controller
         $product->region_id = $request->input('diyu');//地域分类
 
         $product->authorization_id = $request->input('Jszzdm');//授权条件
-        $product->supplier_id = $request->input('supplier_id','');
+        $product->supplier_id = $request->input('supplier_id', '');
         $product->supplier_name = SupplierModel::find($product->supplier_id)->nam;
-        $product->market_price = $request->input('market_price','');
+        $product->market_price = $request->input('market_price', '');
         $product->sale_price = $request->input('sale_price');
         $product->cost_price = $request->input('cost_price');
-        $product->cover_id = $request->input('cover_id','');
-        $product->unit = $request->input('unit','');
+        $product->cover_id = $request->input('cover_id', '');
+        $product->unit = $request->input('unit', '');
         $product->weight = $request->input('weight');
-        $product->summary = $request->input('summary','');
-        $product->content = $request->input('content','');
+        $product->summary = $request->input('summary', '');
+        $product->content = $request->input('content', '');
         $product->type = 1;
         $product->user_id = Auth::user()->id;
-        $product->product_details = $request->input('product_details',0);
-        $product->mode = $request->input('mode','');
+        $product->product_details = $request->input('product_details', 0);
+        $product->mode = $request->input('mode', '');
         $res = $product->save();
 
-        if($res){
-            $assets = AssetsModel::where('random',$request->input('random'))->get();
-            foreach ($assets as $asset){
+        if ($res) {
+            $assets = AssetsModel::where('random', $request->input('random'))->get();
+            foreach ($assets as $asset) {
                 $asset->target_id = $product->id;
                 $asset->type = 1;
                 $asset->save();
             }
-                return redirect('/product/edit?id='.$product->id);
-            }else{
-                return '保存失败';
+            return redirect('/product/edit?id=' . $product->id);
+        } else {
+            return '保存失败';
         }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -200,7 +201,7 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit(Request $request)
@@ -216,26 +217,26 @@ class ProductController extends Controller
 
         $product = ProductsModel::find($id);
 
-        $authorization_id = explode(",",$product->authorization_id);
+        $authorization_id = explode(",", $product->authorization_id);
 
-        $region = explode(",",$product->region_id);
+        $region = explode(",", $product->region_id);
         //获取七牛上传token
         $token = QiniuApi::upToken();
 
         $user_id = Auth::user()->id;
 
         //获取商品的图片
-        $assets = AssetsModel::where(['target_id' => $id,'type' => 1])->get();
+        $assets = AssetsModel::where(['target_id' => $id, 'type' => 1])->get();
         //获取商品详情的图片
-        $assetsProductDetails = AssetsModel::where(['target_id' => $id,'type' => 22])->get();
+        $assetsProductDetails = AssetsModel::where(['target_id' => $id, 'type' => 22])->get();
 
         $random = [];
-        for ($i = 0; $i<2; $i++) {
+        for ($i = 0; $i < 2; $i++) {
             $random[] = uniqid();  //获取唯一字符串
         }
 
         $url = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
-        if(!Cookie::has('product_back_url')){
+        if (!Cookie::has('product_back_url')) {
             Cookie::queue('product_back_url', $url, 60);  // 设置修改完成转跳url
         }
         $this->tab_menu = 'default';
@@ -248,8 +249,8 @@ class ProductController extends Controller
             'product' => $product,
             'lists' => $lists,
             'suppliers' => $suppliers,
-            'authorization' =>$authorization_id,
-            'region' =>$region,
+            'authorization' => $authorization_id,
+            'region' => $region,
             'provinces' => $provinces,
             'token' => $token,
             'user_id' => $user_id,
@@ -276,7 +277,7 @@ class ProductController extends Controller
             'region_id' => 'required',
 //            'supplier_id' => 'required',
             'sale_price' => 'required',
-            'number' => 'required|unique:products,number,'.$request->input('product_id'),
+            'number' => 'required|unique:products,number,' . $request->input('product_id'),
         ];
         $messages = [
             'title.required' => '名称不能为空',
@@ -289,7 +290,7 @@ class ProductController extends Controller
             'number.required' => '货号不能为空',
             'number.unique' => '货号已存在',
         ];
-        $this->validate($request, $rules,$messages);
+        $this->validate($request, $rules, $messages);
         $id = (int)$request->input('product_id');
         $product = ProductsModel::find($id);
 
@@ -299,32 +300,32 @@ class ProductController extends Controller
         $product->tit = $request->input('tit');
         $product->category_id = $request->input('category_id');
         $authorization = $request->input('authorization_id');
-        $product->authorization_id = implode(',',$authorization);
+        $product->authorization_id = implode(',', $authorization);
         $region = $request->input('region_id');
-        $product->region_id = implode(',',$region);
-        $product->supplier_id = $request->input('supplier_id','');
+        $product->region_id = implode(',', $region);
+        $product->supplier_id = $request->input('supplier_id', '');
         $product->supplier_name = SupplierModel::find($product->supplier_id)->nam;
-        $product->market_price = $request->input('market_price','');
+        $product->market_price = $request->input('market_price', '');
         $product->sale_price = $request->input('sale_price');
         $product->cost_price = $request->input('cost_price');
-        $product->cover_id = $request->input('cover_id','');
-        $product->unit = $request->input('unit','');
+        $product->cover_id = $request->input('cover_id', '');
+        $product->unit = $request->input('unit', '');
         $product->weight = $request->input('weight');
-        $product->summary = $request->input('summary','');
+        $product->summary = $request->input('summary', '');
         $product->type = 1;
         $product->user_id = Auth::user()->id;
 
-        $product->content = $request->input('content','');
-        $product->mode = $request->input('mode','');
+        $product->content = $request->input('content', '');
+        $product->mode = $request->input('mode', '');
         $result = $product->update();
 
-        if($result){
+        if ($result) {
 
             $url = Cookie::get('product_back_url');
             Cookie::forget('product_back_url');
 //            return redirect($url);
             return redirect('/product');
-        }else{
+        } else {
             return "更新失败";
         }
     }
@@ -338,23 +339,23 @@ class ProductController extends Controller
     {
         $id_arr = $request->input('id');
 
-        foreach ($id_arr as $id){
+        foreach ($id_arr as $id) {
             $productModel = ProductsModel::find($id);
-            if($productModel->status != 1){
-                return ajax_json(0,'该商品已上架或已取消不能上架');
+            if ($productModel->status != 1) {
+                return ajax_json(0, '该商品已上架或已取消不能上架');
             }
-            if($productModel->productsSku->isEmpty()){
-                return ajax_json(0,'该商品未添加SKU不能上架');
+            if ($productModel->productsSku->isEmpty()) {
+                return ajax_json(0, '该商品未添加SKU不能上架');
             }
-            if($productModel->inventory < 1){
-                return ajax_json(0,'该商品库存为0不能上架');
+            if ($productModel->inventory < 1) {
+                return ajax_json(0, '该商品库存为0不能上架');
             }
-            if(!$productModel->changeProduct(2)){
-                return ajax_json(0,$productModel->tit . '上架失败');
+            if (!$productModel->changeProduct(2)) {
+                return ajax_json(0, $productModel->tit . '上架失败');
             }
         }
 
-        return ajax_json(1,'商品上架成功');
+        return ajax_json(1, '商品上架成功');
     }
 
     /**
@@ -366,17 +367,17 @@ class ProductController extends Controller
     {
         $id_arr = $request->input('id');
 
-        foreach ($id_arr as $id){
+        foreach ($id_arr as $id) {
             $productModel = ProductsModel::find($id);
-            if($productModel->status != 2){
-                return ajax_json(0,'该商品已下架或已取消不能下架');
+            if ($productModel->status != 2) {
+                return ajax_json(0, '该商品已下架或已取消不能下架');
             }
-            if(!$productModel->changeProduct(1)){
-                return ajax_json(0,$productModel->tit . '下架失败');
+            if (!$productModel->changeProduct(1)) {
+                return ajax_json(0, $productModel->tit . '下架失败');
             }
         }
 
-        return ajax_json(1,'商品下架成功');
+        return ajax_json(1, '商品下架成功');
     }
 
     /**
@@ -388,17 +389,17 @@ class ProductController extends Controller
     {
         $id_arr = $request->input('id');
 
-        foreach ($id_arr as $id){
+        foreach ($id_arr as $id) {
             $productModel = ProductsModel::find($id);
-            if($productModel->status == 3){
-                return ajax_json(0,'该商品已取消，不能重复取消');
+            if ($productModel->status == 3) {
+                return ajax_json(0, '该商品已取消，不能重复取消');
             }
-            if(!$productModel->changeProduct(3)){
-                return ajax_json(0,$productModel->tit . '删除失败');
+            if (!$productModel->changeProduct(3)) {
+                return ajax_json(0, $productModel->tit . '删除失败');
             }
         }
 
-        return ajax_json(1,'删除商品成功');
+        return ajax_json(1, '删除商品成功');
     }
 
     /*
@@ -406,30 +407,30 @@ class ProductController extends Controller
      */
     public function search(Request $request)
     {
-        $this->per_page = $request->input('per_page',$this->per_page);
+        $this->per_page = $request->input('per_page', $this->per_page);
         $name = $request->input('search');
         $supplier_id = $request->input('supplier_id') ? $request->input('supplier_id') : 0;
-        if($supplier_id !== 0){
-            $products = ProductsModel::where('supplier_id' , $supplier_id)->paginate($this->per_page);
-        }else{
-            $sku = ProductsSkuModel::where('number' , 'like','%'.$name.'%')->first();
-            if($sku){
-                $products = ProductsModel::where('id', $sku->product_id)->orWhere('title','like','%'.$name.'%')->orWhere('tit','like','%'.$name.'%')->paginate($this->per_page);
-            }else{
-                $products = ProductsModel::where('number','like','%'.$name.'%')->orWhere('title','like','%'.$name.'%')->orWhere('tit','like','%'.$name.'%')->paginate($this->per_page);
+        if ($supplier_id !== 0) {
+            $products = ProductsModel::where('supplier_id', $supplier_id)->paginate($this->per_page);
+        } else {
+            $sku = ProductsSkuModel::where('number', 'like', '%' . $name . '%')->first();
+            if ($sku) {
+                $products = ProductsModel::where('id', $sku->product_id)->orWhere('title', 'like', '%' . $name . '%')->orWhere('tit', 'like', '%' . $name . '%')->paginate($this->per_page);
+            } else {
+                $products = ProductsModel::where('number', 'like', '%' . $name . '%')->orWhere('title', 'like', '%' . $name . '%')->orWhere('tit', 'like', '%' . $name . '%')->paginate($this->per_page);
 
             }
         }
-        $skus = ProductsSkuModel::orderBy('id','desc')->get();
+        $skus = ProductsSkuModel::orderBy('id', 'desc')->get();
         $skuId = [];
-        foreach($skus as $sku){
+        foreach ($skus as $sku) {
             $skuId[] = $sku->product_id;
         }
         $suppliersModel = new SupplierModel();
         $suppliers = $suppliersModel->supplierList();
-        if ($products){
-            return view('home/product.home',[
-                'products'=>$products,
+        if ($products) {
+            return view('home/product.home', [
+                'products' => $products,
                 'tab_menu' => $this->tab_menu,
                 'skuId' => $skuId,
                 'name' => $name,
@@ -477,17 +478,17 @@ class ProductController extends Controller
         $user_id = Auth::user()->id;
 
         //获取商品的图片
-        $assets = AssetsModel::where(['target_id' => $id,'type' => 1])->get();
+        $assets = AssetsModel::where(['target_id' => $id, 'type' => 1])->get();
         //获取商品详情的图片
-        $assetsProductDetails = AssetsModel::where(['target_id' => $id,'type' => 22])->get();
+        $assetsProductDetails = AssetsModel::where(['target_id' => $id, 'type' => 22])->get();
 
         $random = [];
-        for ($i = 0; $i<2; $i++) {
+        for ($i = 0; $i < 2; $i++) {
             $random[] = uniqid();  //获取唯一字符串
         }
 
         $url = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
-        if(!Cookie::has('product_back_url')){
+        if (!Cookie::has('product_back_url')) {
             Cookie::queue('product_back_url', $url, 60);  // 设置修改完成转跳url
         }
         return view('home/product.details', [
@@ -513,27 +514,27 @@ class ProductController extends Controller
         $storage_id = config('constant.storage_id');
         $sku_number = config('constant.sku_count');
         //获取sku信息
-        $product_sku = ProductsSkuModel::where('id' , $sku_id)->first();
-        if(!$product_sku){
-            return ajax_json(0,'没有找到sku');
+        $product_sku = ProductsSkuModel::where('id', $sku_id)->first();
+        if (!$product_sku) {
+            return ajax_json(0, '没有找到sku');
         }
         // 增加商品，SKU 总库存
         $skuModel = new ProductsSkuModel();
-        if(!$skuModel->addInventory($sku_id, $sku_number)){
-            return ajax_json(0 , '虚拟库存变更数量失败');
+        if (!$skuModel->addInventory($sku_id, $sku_number)) {
+            return ajax_json(0, '虚拟库存变更数量失败');
         }
         //查看虚拟库存根据sku_id,仓库id，部门是否创建过
-        $storage_sku_count = StorageSkuCountModel::where('sku_id' , $sku_id)->where('storage_id' , $storage_id)->where('department' , 1)->first();
+        $storage_sku_count = StorageSkuCountModel::where('sku_id', $sku_id)->where('storage_id', $storage_id)->where('department', 1)->first();
         //创建过变更为10000
-        if($storage_sku_count){
+        if ($storage_sku_count) {
             $storage_sku_count->count = $sku_number + $storage_sku_count->count;
-            if($storage_sku_count->save()){
-                return ajax_json(1 , '该虚拟库存已经存在，补充完毕');
-            }else{
-                return ajax_json(0 , '虚拟库存变更数量失败');
+            if ($storage_sku_count->save()) {
+                return ajax_json(1, '该虚拟库存已经存在，补充完毕');
+            } else {
+                return ajax_json(0, '虚拟库存变更数量失败');
             }
-        //没创建时重新生成创建
-        }else{
+            //没创建时重新生成创建
+        } else {
             $storage_skuCount = new StorageSkuCountModel();
             $storage_skuCount->sku_id = $sku_id;
             $storage_skuCount->storage_id = $storage_id;
@@ -541,10 +542,10 @@ class ProductController extends Controller
             $storage_skuCount->count = $sku_number;
             $storage_skuCount->product_id = $product_sku->product_id;
             $storage_skuCount->product_number = $product_sku->product_number;
-            if($storage_skuCount->save()){
-                return ajax_json(1 , '虚拟库存生成成功');
-            }else{
-                return ajax_json(0 , '虚拟库存生成失败');
+            if ($storage_skuCount->save()) {
+                return ajax_json(1, '虚拟库存生成成功');
+            } else {
+                return ajax_json(0, '虚拟库存生成失败');
             }
         }
     }
