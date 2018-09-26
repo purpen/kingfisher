119521fd02e0f4cc1b9e26b787128fada59905d4
liujiadong <1403417121@qq.com>
2018-09-26 10:53:51 +0800

    修改样式

--- resources/views/mustache/invoice_info.blade.php
+++ resources/views/mustache/invoice_info.blade.php
@@ -271,7 +271,7 @@
                 </div>
 
     </div>
- 
+
     <div class="form-group">
         <label class="col-sm-1 control-label">发票状态</label>
         <div class="col-sm-3">
@@ -350,9 +350,10 @@
                     <span class="form-text text-danger">@{{ receiving_address }}</span>
                 </div> 
 	    @{{ #prove }}
-				 <label class="col-sm-1 control-label" style="margin-left: 40%;margin-top: -20%;width:120px;">一般纳税人证明</label>
-                <div class="col-sm-3">
-                     <a href="@{{prove_id}}"  target="_blank"><img src="@{{prove_id}}" alt="100x100" class="img-thumbnail" style="height: 100px; width: 100px;margin-left: 220%;margin-top: -120%;"></a>
+    <div class="col-sm-3">
+                <label class="col-sm-1 control-label" style="width:120px;">一般纳税人证明</label>
+
+                    <a href="@{{prove_id}}"  target="_blank"><img src="@{{prove_id}}" alt="100x100" class="img-thumbnail" style="height: 100px; width: 100px; "></a>
                 
                 </div> 
 		@{{ /prove }} 
