c135014dbf72d3332cac8353bce6dc90afcb5940
liujiadong <1403417121@qq.com>
2018-09-29 14:34:01 +0800

    修改订单审核时提示

--- app/Http/Controllers/Home/OrderController.php
+++ app/Http/Controllers/Home/OrderController.php
@@ -650,6 +650,9 @@ class OrderController extends Controller
             }
             $department = UserModel::find($order_model->user_id_sales) ? UserModel::find($order_model->user_id_sales)->department : '';
             $storage_sku = new StorageSkuCountModel();
+            if (!$storage_id){
+                return ajax_json(0,'请先选择发货仓库,再审核');
+            }
             if(!$storage_sku->isCount($storage_id, $department, $sku_id_arr, $sku_count_arr)){
                 DB::rollBack();
                 return ajax_json(0,'发货商品所选仓库/部门库存不足');
