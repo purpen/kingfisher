dafe2a18debd6aa6ccdbebf936d15bb28623d586
llh <liheng666@foxmail.com>
2018-10-11 14:49:00 +0800

    收款单统计

--- app/Http/Controllers/Home/ReceiveOrderController.php
+++ app/Http/Controllers/Home/ReceiveOrderController.php
@@ -126,7 +126,8 @@ class ReceiveOrderController extends Controller
     {
         $receive = ReceiveOrderModel::where('status', 0)->paginate($this->per_page);
         $money = ReceiveOrderModel
-            ::where('type', 3)
+            ::where('status', 0)
+            ->where('type', 3)
             ->select(DB::raw('sum(amount) as amount_sum,sum(received_money) as received_sum '))
             ->first();
         $message = DB::table('order')
@@ -157,7 +158,8 @@ class ReceiveOrderController extends Controller
     {
         $receive = ReceiveOrderModel::where('status', 1)->paginate($this->per_page);
         $money = ReceiveOrderModel
-            ::where('type', 3)
+            ::where('status', 1)
+            ->where('type', 3)
             ->select(DB::raw('sum(amount) as amount_sum,sum(received_money) as received_sum '))
             ->first();
 
