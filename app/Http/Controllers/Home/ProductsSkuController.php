dc04295ba7f9117c0d683599da7099a781dd845e
liujiadong <1403417121@qq.com>
2018-11-01 11:29:26 +0800

    修改添加商品时搜索问题

--- app/Http/Controllers/Home/ProductsSkuController.php
+++ app/Http/Controllers/Home/ProductsSkuController.php
@@ -306,7 +306,7 @@ class ProductsSkuController extends Controller
         $where = $request->input('where');
         $supplier_id = $request->input('supplier_id');
         $productsSku = new ProductsSkuModel();
-        $skus = $productsSku->lists($where,$supplier_id);
+        $skus = $productsSku->listOf($where,'');
         if(empty($skus)){
             return ajax_json(0,'error','未查询到相关商品');
         }
