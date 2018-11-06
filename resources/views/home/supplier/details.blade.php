c0fcfa5218413ff9f3f9312fa41a19f495b31279
dr <2834144959@qq.com>
2018-11-06 10:57:54 +0800

    供应商修改

--- resources/views/home/supplier/details.blade.php
+++ resources/views/home/supplier/details.blade.php
@@ -53,14 +53,14 @@
                             <h5>公司信息</h5>
                             <hr>
                             <ul class="form-group clearfix" style="list-style-type:none;line-height: 30px;">
-                                <li for="name" class="mb-0r control-label col-md-6"><b>公司全称:</b>{{ $supplier->name }}</li>
-                                <li for="nam" class="mb-0r control-label col-md-6"><b>简称:</b>{{ $supplier->nam }}</li>
-                                <li for="address" class="mb-0r control-label col-md-6"><b>地址:</b>{{ $supplier->address }}</li>
-                                <li for="legal_person" class="mb-0r control-label col-md-6"><b>法人:</b>{{ $supplier->legal_person }}</li>
-                                <li for="ein" class="mb-0r control-label col-md-6"><b>税号:</b>{{ $supplier->ein }}</li>
-                                <li for="tax_rate" class="mb-0r control-label col-md-6"><b>开票税率:</b>{{ $supplier->tax_rate }}</li>
-                                <li for="bank_number" class="mb-0r control-label col-md-6"><b>开户行号:</b>{{ $supplier->bank_number }}</li>
-                                <li for="bank_address" class="mb-0r control-label col-md-6"><b>开户行名称:</b>{{ $supplier->bank_address }}</li>
+                                <li for="name" class="mb-0r control-label col-md-6"><b>公司全称:</b>{{ $supplier->name ? $supplier->name :''}}</li>
+                                <li for="nam" class="mb-0r control-label col-md-6"><b>简称:</b>{{ $supplier->nam ? $supplier->nam :''}}</li>
+                                <li for="address" class="mb-0r control-label col-md-6"><b>地址:</b>{{ $supplier->address ? $supplier->address :''}}</li>
+                                <li for="legal_person" class="mb-0r control-label col-md-6"><b>法人:</b>{{ $supplier->legal_person ? $supplier->legal_person :''}}</li>
+                                <li for="ein" class="mb-0r control-label col-md-6"><b>税号:</b>{{ $supplier->ein ? $supplier->ein :'' }}</li>
+                                <li for="tax_rate" class="mb-0r control-label col-md-6"><b>开票税率:</b>{{ $supplier->tax_rate ? $supplier->tax_rate :''}}</li>
+                                <li for="bank_number" class="mb-0r control-label col-md-6"><b>开户行号:</b>{{ $supplier->bank_number ? $supplier->bank_number :'' }}</li>
+                                <li for="bank_address" class="mb-0r control-label col-md-6"><b>开户行名称:</b>{{ $supplier->bank_address ? $supplier->bank_address:'' }}</li>
                                 <li for="start_time" class="mb-0r control-label col-md-6"><b>合作开始时间:</b>{{ $supplier->start_time == '0000-00-00' ? '' : $supplier->start_time }}</li>
                                 <li for="end_time" class="mb-0r control-label col-md-6"><b>合作结束时间:</b>{{ $supplier->end_time == '0000-00-00' ? '' : $supplier->end_time }}</li>
                                 <li for="authorization_deadline" class="mb-0r control-label col-md-6"><b>授权期限:</b>{{ $supplier->authorization_deadline == '0000-00-00' ? '' : $supplier->authorization_deadline }}</li>
@@ -71,12 +71,12 @@
                             <hr>
 
                             <ul class="form-group clearfix" style="list-style-type:none;line-height: 30px;">
-                                <li for="contact_user" class="mb-0r control-label col-md-6"><b>联系人:</b>{{ $supplier->contact_user }}</li>
-                                <li for="contact_number" class="mb-0r control-label col-md-6"><b>联系人手机:</b>{{ $supplier->contact_number }}</li>
-                                <li for="contact_email" class="mb-0r control-label col-md-6"><b>联系邮箱:</b>{{ $supplier->contact_email }}</li>
-                                <li for="contact_wx" class="mb-0r control-label col-md-6"><b>联系人微信:</b>{{ $supplier->contact_wx }}</li>
-                                <li for="contact_qq" class="mb-0r control-label col-md-6"><b>联系人QQ:</b>{{ $supplier->contact_qq }}</li>
-                                <li for="tel" class="mb-0r control-label col-md-6"><b>联系人电话:</b>{{ $supplier->tel }}</li>
+                                <li for="contact_user" class="mb-0r control-label col-md-6"><b>联系人:</b>{{ $supplier->contact_user ? $supplier->contact_user :'' }}</li>
+                                <li for="contact_number" class="mb-0r control-label col-md-6"><b>联系人手机:</b>{{ $supplier->contact_number ? $supplier->contact_number :'' }}</li>
+                                <li for="contact_email" class="mb-0r control-label col-md-6"><b>联系邮箱:</b>{{ $supplier->contact_email ? $supplier->contact_email :'' }}</li>
+                                <li for="contact_wx" class="mb-0r control-label col-md-6"><b>联系人微信:</b>{{ $supplier->contact_wx ? $supplier->contact_wx :'' }}</li>
+                                <li for="contact_qq" class="mb-0r control-label col-md-6"><b>联系人QQ:</b>{{ $supplier->contact_qq ? $supplier->contact_qq :'' }}</li>
+                                <li for="tel" class="mb-0r control-label col-md-6"><b>联系人电话:</b>{{ $supplier->tel ? $supplier->tel :'' }}</li>
                             </ul>
 
                             <h5>图片信息</h5>
@@ -84,24 +84,25 @@
                             <ul class="form-group clearfix" style="list-style-type:none;line-height: 30px;">
                                 <li for="cover_id" class="mb-0r control-label col-md-6"><b>pdf附件(电子版合同):</b>
                                     @if($supplier->first_asset)
-                                        <a href="{{ $supplier->first_asset}}" target="_blank">{{$supplier->assets->file->name}}</a>
+{{--                                        <a href="{{ $supplier->first_asset}}" target="_blank">{{$supplier->assets->file->name}}</a>--}}
+                                        <a href="{{ $supplier->first_asset}}" target="_blank">{{$supplier->assets ? $supplier->assets->file->name :''}}</a>
                                     @endif
                                 </li>
                                 <li for="trademark_id" class="mb-0r control-label col-md-6"><b>商标:</b>
                                     @if($supplier->first_trademark)
-                                        <a href="{{$supplier->first_trademark}}" target="_blank">{{$supplier->assetsTrademark->file->name}}</a>
+                                        <a href="{{$supplier->first_trademark}}" target="_blank">{{$supplier->assetsTrademark ? $supplier->assetsTrademark->file->name :''}}</a>
                                     @endif
                                 </li>
 
                                 <li for="power_of_attorney_id" class="mb-0r control-label col-md-6"><b>授权书:</b>
                                     @if($supplier->first_power_of_attorney)
-                                        <a href="{{$supplier->first_power_of_attorney}}" target="_blank">{{$supplier->assetsPowerOfAttorney->file->name}}</a>
+                                        <a href="{{$supplier->first_power_of_attorney}}" target="_blank">{{$supplier->assetsPowerOfAttorney ? $supplier->assetsPowerOfAttorney->file->name :''}}</a>
                                     @endif
                                 </li>
 
                                 <li for="quality_inspection_report_id" class="mb-0r control-label col-md-6"><b>质检报告:</b>
                                     @if($supplier->first_quality_inspection_report)
-                                        <a href="{{$supplier->first_quality_inspection_report}}" target="_blank">{{$supplier->assetsQualityInspectionReport->file->name}}</a>
+                                        <a href="{{$supplier->first_quality_inspection_report}}" target="_blank">{{$supplier->assetsQualityInspectionReport ? $supplier->assetsQualityInspectionReport->file->name :''}}</a>
                                     @endif
                                 </li>
 
