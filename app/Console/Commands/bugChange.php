fa7b526b58f8338847db739bf8af3cb0b47c8847
llh <liheng666@foxmail.com>
2018-11-07 21:44:45 +0800

    debug

--- app/Console/Commands/bugChange.php
+++ app/Console/Commands/bugChange.php
@@ -42,62 +42,61 @@ class bugChange extends Command
     public function handle()
     {
 
-        $d = [
-            'RKCG2018110500005',
-        ];
-
-        try {
-            DB::beginTransaction();
-            foreach ($d as $v) {
-                $ew = EnterWarehousesModel::query()->where("number", $v)->first();
-                if (!$ew) {
-                    continue;
-                }
-                $p = $ew->purchase;
-                $p->in_count = $p->count; // 减少对应采购单入库数量
-                $p->save();
-
-                $ps = $p->purchaseSku;
-                foreach ($ps as $p1) {
-                    $p1->in_count = $p1->count;  // 采购单明细
-                    $p1->save();
-                }
-
-                $storage_id = $ew->storage_id;
-                $ewhs = $ew->enterWarehouseSkus;
-                foreach ($ewhs as $ewh) {
-                    $sku_id = $ewh->sku_id;
-
-                    $sscm = StorageSkuCountModel::query()
-                        ->where("sku_id", $sku_id)
-                        ->where("storage_id", $storage_id)
-                        ->first();
-                    if ($sscm) {  // 仓库sku
-                        $sscm->count = $sscm->count - $ewh->in_count;
-                        $sscm->save();
-                    }
-
-                    $sku = ProductsSkuModel::find($sku_id);
-                    if ($sku) {
-                        $sku->quantity = $sku->quantity - $ewh->in_count;  //sku
-                        $sku->save();
-
-                        $product = $sku->product;
-                        $product->inventory = $product->inventory - $ewh->in_count; // product
-                        $product->save();
-                    }
-                    $ewh->forceDelete();
-                }
-                $ew->forceDelete();
-            }
-
-
-        } catch (\Exception $e) {
-            DB::rollBack();
-            $this->info($e->getCode() . $e->getMessage());
-        }
-        DB::commit();
-        $this->info("ok");
+//        $d = [
+//        ];
+//
+//        try {
+//            DB::beginTransaction();
+//            foreach ($d as $v) {
+//                $ew = EnterWarehousesModel::query()->where("number", $v)->first();
+//                if (!$ew) {
+//                    continue;
+//                }
+//                $p = $ew->purchase;
+//                $p->in_count = $p->count; // 减少对应采购单入库数量
+//                $p->save();
+//
+//                $ps = $p->purchaseSku;
+//                foreach ($ps as $p1) {
+//                    $p1->in_count = $p1->count;  // 采购单明细
+//                    $p1->save();
+//                }
+//
+//                $storage_id = $ew->storage_id;
+//                $ewhs = $ew->enterWarehouseSkus;
+//                foreach ($ewhs as $ewh) {
+//                    $sku_id = $ewh->sku_id;
+//
+//                    $sscm = StorageSkuCountModel::query()
+//                        ->where("sku_id", $sku_id)
+//                        ->where("storage_id", $storage_id)
+//                        ->first();
+//                    if ($sscm) {  // 仓库sku
+//                        $sscm->count = $sscm->count - $ewh->in_count;
+//                        $sscm->save();
+//                    }
+//
+//                    $sku = ProductsSkuModel::find($sku_id);
+//                    if ($sku) {
+//                        $sku->quantity = $sku->quantity - $ewh->in_count;  //sku
+//                        $sku->save();
+//
+//                        $product = $sku->product;
+//                        $product->inventory = $product->inventory - $ewh->in_count; // product
+//                        $product->save();
+//                    }
+//                    $ewh->forceDelete();
+//                }
+//                $ew->forceDelete();
+//            }
+//
+//
+//        } catch (\Exception $e) {
+//            DB::rollBack();
+//            $this->info($e->getCode() . $e->getMessage());
+//        }
+//        DB::commit();
+//        $this->info("ok");
     }
 
 }
